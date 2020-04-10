3ed0fbae39e395833015dd22f4d4c60ab06163ea
GregoryConrad <gregorysconrad@gmail.com>
2020-04-10 01:13:35 -0400

    Added csv prototype starter code

---
+++ src/main/java/edu/wpi/onyx_ouroboros/model/data/CSVHandlerBase.java
@@ -0,0 +1,36 @@
+package edu.wpi.onyx_ouroboros.model.data;
+
+import lombok.Data;
+
+/**
+ * Handles parsing and exporting PrototypeNode CSV files
+ */
+@Data
+public abstract class CSVHandlerBase {
+
+  /**
+   * The database to import records into and export records from
+   */
+  private final IDatabaseWrapper database;
+
+  /**
+   * Imports the given CSV file into the database
+   *
+   * @param csvFileLocation the path of the csv file to read
+   */
+  public abstract void importToDatabase(String csvFileLocation);
+
+  /**
+   * Exports the database into the given CSV file
+   *
+   * @param csvFileLocation the path of the csv file to create/overwrite
+   */
+  public abstract void exportFromDatabase(String csvFileLocation);
+
+  /**
+   * @return the database to use in subclasses
+   */
+  protected IDatabaseWrapper getDatabase() {
+    return database;
+  }
+}
