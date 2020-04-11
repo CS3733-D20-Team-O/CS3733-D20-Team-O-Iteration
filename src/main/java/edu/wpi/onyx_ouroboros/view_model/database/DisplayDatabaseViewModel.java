6450947cb7d8633a7563b5fe89c3d452ede0032b
AzinxtheOnix <mzlai@wpi.edu>
2020-04-11 02:21:35 -0400

    Made some slight organization changes

--- src/main/java/edu/wpi/onyx_ouroboros/view_model/database/DisplayDatabaseViewModel.java
+++ src/main/java/edu/wpi/onyx_ouroboros/view_model/database/DisplayDatabaseViewModel.java
@@ -1,23 +1,25 @@
-package edu.wpi.onyx_ouroboros.view_model;
+package edu.wpi.onyx_ouroboros.view_model.database;
 
 import edu.wpi.onyx_ouroboros.model.data.database.events.DatabaseNodeDeletedEvent;
 import edu.wpi.onyx_ouroboros.model.data.database.events.DatabaseNodeInsertedEvent;
 import edu.wpi.onyx_ouroboros.model.data.database.events.DatabaseNodeUpdatedEvent;
+import edu.wpi.onyx_ouroboros.view_model.ViewModelBase;
 import org.greenrobot.eventbus.Subscribe;
 
 public class DisplayDatabaseViewModel extends ViewModelBase {
+
   @Subscribe
   public void onNodeDeleted(DatabaseNodeDeletedEvent event) {
-
+    // todo finish the onNodeDeleted event
   }
 
   @Subscribe
   public void onNodeInsert(DatabaseNodeInsertedEvent event) {
-
+    // todo finish the onNodeInsert event
   }
 
   @Subscribe
   public void onNodeDeleted(DatabaseNodeUpdatedEvent event) {
-
+    // todo finish the onNodeDeleted event
   }
 }
--- src/main/java/edu/wpi/onyx_ouroboros/view_model/database/ModifyDatabaseViewModel.java
+++ src/main/java/edu/wpi/onyx_ouroboros/view_model/database/ModifyDatabaseViewModel.java
@@ -1,10 +1,12 @@
-package edu.wpi.onyx_ouroboros.view_model;
+package edu.wpi.onyx_ouroboros.view_model.database;
 
 import edu.wpi.onyx_ouroboros.model.DependencyInjector;
 import edu.wpi.onyx_ouroboros.model.data.csv.CSVHandler;
 import edu.wpi.onyx_ouroboros.model.data.database.DatabaseWrapper;
+import edu.wpi.onyx_ouroboros.view_model.ViewModelBase;
 
 public class ModifyDatabaseViewModel extends ViewModelBase {
+
   public void onAddClicked() {
     DependencyInjector.create(DatabaseWrapper.class).addNode(null);
     DependencyInjector.create(DatabaseWrapper.class).deleteNode(null);
