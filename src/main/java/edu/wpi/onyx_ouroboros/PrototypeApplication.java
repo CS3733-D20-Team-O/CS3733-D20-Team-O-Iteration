10ca1da18a6486acf6706ef7fba2f5855f2b419c
Mark Hogan <mhogan2@wpi.edu>
2020-04-12 11:52:53 -0400

    modified ProtostypeApplication to open 4 unique windows.

--- src/main/java/edu/wpi/onyx_ouroboros/PrototypeApplication.java
+++ src/main/java/edu/wpi/onyx_ouroboros/PrototypeApplication.java
@@ -2,15 +2,20 @@ package edu.wpi.onyx_ouroboros;
 
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
+
 @Slf4j
 public class PrototypeApplication extends Application {
+
+  private String resourceLoc = "resources/edu/wpi/onyx_ouroboros/views/prototype/";
 
   @Override
   public void init() {
@@ -20,11 +25,31 @@ public class PrototypeApplication extends Application {
   @Override
   public void start(Stage primaryStage) throws IOException {
     // todo load all 4 windows from views/prototype/*.fxml
-    val loader = new FXMLLoader(getClass().getResource("views/Main.fxml"));
-    val root = (Parent) loader.load();
-    val viewModel = loader.getController();
-    primaryStage.setScene(new Scene(root));
-    primaryStage.show();
+
+    String dataDisplayRoot = "views/prototype/DisplayDatabase.fxml";
+    String dataDisplayTitle = "Database Display";
+    String dataModifyRoot = "views/prototype/ModifyDatabase.fxml";
+    String dataModifyTitle = "Database Modify";
+    String dataDownloadRoot = "views/prototype/DownloadDatabase.fxml";
+    String dataDownloadTitle = "Database Download";
+    String astarDemoRoot = "views/prototype/AStar.fxml";
+    String astarDemoTitle = "A* Demo Display";
+
+    openWindow(dataDisplayRoot, dataDisplayTitle);
+    openWindow(dataModifyRoot, dataModifyTitle);
+    openWindow(dataDownloadRoot, dataDownloadTitle);
+    openWindow(astarDemoRoot, astarDemoTitle);
+
+  }
+
+  private void openWindow(String rootLoc, String stageTitle) throws IOException {
+    Parent displayRoot = FXMLLoader.load(getClass().getResource(rootLoc));
+    val displayStage = new Stage();
+    val displayScene = new Scene(displayRoot);
+    displayStage.setScene(displayScene);
+    displayStage.setTitle(stageTitle);
+    displayStage.show();
+
   }
 
   @Override
