1615ead2de7e6720fd64d91f9056cb94559c3976
Collin Broderick <sedrahedron@gmail.com>
2020-04-12 15:25:28 -0400

    Started progress

--- src/main/java/edu/wpi/onyx_ouroboros/view_model/prototype/AStarViewModel.java
+++ src/main/java/edu/wpi/onyx_ouroboros/view_model/prototype/AStarViewModel.java
@@ -1,7 +1,15 @@
 package edu.wpi.onyx_ouroboros.view_model.prototype;
 
+// drawing circles and lines to represent nodes and edges
+
 import edu.wpi.onyx_ouroboros.view_model.ViewModelBase;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 public class AStarViewModel extends ViewModelBase {
   // todo
+  @FXML
+  private void generatePath(ActionEvent event) {
+    System.out.println("Clicked!");
+  }
 }
