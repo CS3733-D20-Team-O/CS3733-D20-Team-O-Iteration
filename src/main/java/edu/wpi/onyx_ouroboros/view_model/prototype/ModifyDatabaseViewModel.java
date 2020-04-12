d37ce0046b8d2a8754044ac98add56b7ae1d2a85
Benny Klaiman <bnklaiman@wpi.edu>
2020-04-12 13:18:10 -0400

    first changes to issue 32

--- src/main/java/edu/wpi/onyx_ouroboros/view_model/prototype/ModifyDatabaseViewModel.java
+++ src/main/java/edu/wpi/onyx_ouroboros/view_model/prototype/ModifyDatabaseViewModel.java
@@ -2,9 +2,21 @@ package edu.wpi.onyx_ouroboros.view_model.prototype;
 
 import edu.wpi.onyx_ouroboros.model.DependencyInjector;
 import edu.wpi.onyx_ouroboros.model.data.database.DatabaseWrapper;
+import edu.wpi.onyx_ouroboros.model.data.PrototypeNode;
 import edu.wpi.onyx_ouroboros.view_model.ViewModelBase;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class ModifyDatabaseViewModel extends ViewModelBase {
+
+  @FXML private Button addNodeBtn;
+  @FXML private Button updateNodeBtn;
+  @FXML private Button deleteNodeBtn;
+  @FXML private TextField addNodeField;
+  @FXML private TextField updateNodeFieldID;
+  @FXML private TextField updateNodeFieldNode;
+  @FXML private TextField deleteNodeField;
 
   private final DatabaseWrapper database = DependencyInjector.create(DatabaseWrapper.class);
 
@@ -13,18 +25,40 @@ public class ModifyDatabaseViewModel extends ViewModelBase {
   // Thus, feel free to change the method names and the parameters they take below, but make sure
   // this class provides the appropriate functionality (add, update, & delete)
 
-  public void addNode() {
-    // todo add node to database
-    // database.addNode(prototypeNode); may be helpful
+  @FXML
+  public void addNode(PrototypeNode node) {
+    if (addNodeField.equals("")) {
+      System.out.println("An error occurred when attempting to add a node.\nThe field must not be empty!");
+    } else {
+      node = (PrototypeNode) addNodeField.getCharacters();
+      // add node to database
+      database.addNode(node);
+    }
   }
 
-  public void updateNode() {
-    // todo update node in database
-    // database.updateNode(nodeID, prototypeNode); may be helpful
+  @FXML
+  public void updateNode(String nodeID, PrototypeNode node) {
+    if (updateNodeFieldID.equals("") || updateNodeFieldNode.equals("")) {
+      System.out.println("An error occurred when attempting to update a node.");
+      if (updateNodeFieldID.equals("")) {
+        System.out.println("The ID field must not be empty!");
+      }
+      if (updateNodeFieldNode.equals("")) {
+        System.out.println("The node field must not be empty!");
+      }
+    } else {
+      // todo update node in database
+      database.updateNode(nodeID, node);
+    }
   }
 
-  public void deleteNode() {
-    // todo delete node from database
-    // database.deleteNode(nodeID); may be helpful
+  @FXML
+  public void deleteNode(String nodeID) {
+    if (deleteNodeField.equals("")) {
+      System.out.println("An error occurred when attempting to delete a node.\nThe field must not be empty!");
+    } else {
+      // delete node from database
+      database.deleteNode(nodeID);
+    }
   }
 }
