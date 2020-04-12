2bd5e8cc49d879222bd92ed5cbdb4655557e5899
Mark Hogan <mhogan2@wpi.edu>
2020-04-12 12:41:10 -0400

    changes per PR review

--- src/main/java/edu/wpi/onyx_ouroboros/PrototypeApplication.java
+++ src/main/java/edu/wpi/onyx_ouroboros/PrototypeApplication.java
@@ -2,20 +2,15 @@ package edu.wpi.onyx_ouroboros;
 
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
-
 @Slf4j
 public class PrototypeApplication extends Application {
-
-
 
   @Override
   public void init() {
@@ -24,7 +19,6 @@ public class PrototypeApplication extends Application {
 
   @Override
   public void start(Stage primaryStage) throws IOException {
-    // todo load all 4 windows from views/prototype/*.fxml
 
     val dataDisplayRoot = "views/prototype/DisplayDatabase.fxml";
     val dataDisplayTitle = "Database Display";
@@ -39,7 +33,6 @@ public class PrototypeApplication extends Application {
     openWindow(dataModifyRoot, dataModifyTitle);
     openWindow(dataDownloadRoot, dataDownloadTitle);
     openWindow(astarDemoRoot, astarDemoTitle);
-
   }
 
   private void openWindow(String fxmlLocation, String stageTitle) throws IOException {
@@ -49,7 +42,6 @@ public class PrototypeApplication extends Application {
     displayStage.setScene(displayScene);
     displayStage.setTitle(stageTitle);
     displayStage.show();
-
   }
 
   @Override
