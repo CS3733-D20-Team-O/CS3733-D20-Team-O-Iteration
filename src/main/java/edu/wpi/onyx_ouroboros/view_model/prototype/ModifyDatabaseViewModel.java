3523048b1a250241ea2aa07d16a2256d2d4a60ec
Benny Klaiman <bnklaiman@wpi.edu>
2020-04-14 10:46:41 -0400

    addNode finished

--- src/main/java/edu/wpi/onyx_ouroboros/view_model/prototype/ModifyDatabaseViewModel.java
+++ src/main/java/edu/wpi/onyx_ouroboros/view_model/prototype/ModifyDatabaseViewModel.java
@@ -7,16 +7,26 @@ import edu.wpi.onyx_ouroboros.view_model.ViewModelBase;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import java.lang.CharSequence;
 
 public class ModifyDatabaseViewModel extends ViewModelBase {
 
   @FXML private Button addNodeBtn;
+  @FXML private TextField addIDField;
+  @FXML private TextField addXCoordField;
+  @FXML private TextField addYCoordField;
+  @FXML private TextField addFloorField;
+  @FXML private TextField addBuildingField;
+  @FXML private TextField addNodeTypeField;
+  @FXML private TextField addLongNameField;
+  @FXML private TextField addShortNameField;
+
   @FXML private Button updateNodeBtn;
+  @FXML private TextField updateNodeIDField;
+  @FXML private TextField updateNodeNodeField;
+
   @FXML private Button deleteNodeBtn;
-  @FXML private TextField addNodeField;
-  @FXML private TextField updateNodeFieldID;
-  @FXML private TextField updateNodeFieldNode;
-  @FXML private TextField deleteNodeField;
+  @FXML private TextField deleteNodeIDField;
 
   private final DatabaseWrapper database = DependencyInjector.create(DatabaseWrapper.class);
 
@@ -25,23 +35,52 @@ public class ModifyDatabaseViewModel extends ViewModelBase {
   // Thus, feel free to change the method names and the parameters they take below, but make sure
   // this class provides the appropriate functionality (add, update, & delete)
 
+  // change println to log.(other stuff)
+  // bind button press to fns
+
   @FXML
   public void addNode(PrototypeNode node) {
-    if (addNodeField.equals("")) {
+    if (addIDField.equals("")) {
       System.out.println("An error occurred when attempting to add a node.\nThe field must not be empty!");
     } else {
-      database.addNode(node);
+      // haha data parsing go brrrrr
+      CharSequence idCS = addIDField.getCharacters();
+      String nodeID = idCS.toString();
+
+      CharSequence xCS, yCS, floorCS;
+      int x, y, floor;
+      xCS = addXCoordField.getCharacters();
+      x = Integer.parseInt(xCS, 0, xCS.length(), 10);
+      yCS = addYCoordField.getCharacters();
+      y = Integer.parseInt(yCS, 0, yCS.length(), 10);
+      floorCS = addFloorField.getCharacters();
+      floor = Integer.parseInt(floorCS, 0, floorCS.length(), 10);
+
+      CharSequence buildingCS = null;
+      CharSequence nodeTypeCS = null;
+      CharSequence longNameCS = null;
+      CharSequence shortNameCS = null;
+      String building = buildingCS.toString();
+      String nodeType = nodeTypeCS.toString();
+      String longName = longNameCS.toString();
+      String shortName = shortNameCS.toString();
+
+      // fill in the data from text fields
+      PrototypeNode aNode = new PrototypeNode(nodeID, x, y, floor, building, nodeType, longName, shortName);
+
+      // add node to database
+      database.addNode(aNode);
     }
   }
 
   @FXML
   public void updateNode(String nodeID, PrototypeNode node) {
-    if (updateNodeFieldID.equals("") || updateNodeFieldNode.equals("")) {
+    if (updateNodeIDField.equals("") || updateNodeNodeField.equals("")) {
       System.out.println("An error occurred when attempting to update a node.");
-      if (updateNodeFieldID.equals("")) {
+      if (updateNodeIDField.equals("")) {
         System.out.println("The ID field must not be empty!");
       }
-      if (updateNodeFieldNode.equals("")) {
+      if (updateNodeNodeField.equals("")) {
         System.out.println("The node field must not be empty!");
       }
     } else {
@@ -52,7 +91,7 @@ public class ModifyDatabaseViewModel extends ViewModelBase {
 
   @FXML
   public void deleteNode(String nodeID) {
-    if (deleteNodeField.equals("")) {
+    if (deleteNodeIDField.equals("")) {
       System.out.println("An error occurred when attempting to delete a node.\nThe field must not be empty!");
     } else {
       // delete node from database
