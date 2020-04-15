79037ec1a0dccd3c2dcf55b2806112e57b0527b5
Victoria Grasso <vygrasso@wpi.edu>
2020-04-14 21:26:10 -0400

    Fixes #20

--- src/test/java/edu/wpi/onyx_ouroboros/model/data/database/DatabaseWrapperTest.java
+++ src/test/java/edu/wpi/onyx_ouroboros/model/data/database/DatabaseWrapperTest.java
@@ -1,11 +1,13 @@
 package edu.wpi.onyx_ouroboros.model.data.database;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import edu.wpi.onyx_ouroboros.model.TestInjector;
 import edu.wpi.onyx_ouroboros.model.data.PrototypeNode;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,141 +15,94 @@ import org.junit.jupiter.api.Test;
 /**
  * Tests DatabaseWrapper
  */
+@Slf4j
 public class DatabaseWrapperTest {
 
-  private final static DatabaseWrapperImpl theDatabase = TestInjector
+  private final DatabaseWrapper database = TestInjector
       .create(DatabaseWrapperImpl.class);
+  private final PrototypeNode[] testNodes = {
+      new PrototypeNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1", "Elev1"),
+      new PrototypeNode("0102", 9, 0, 1, "Faulkner", "ELEV", "Elevator 2", "Elev2"),
+      new PrototypeNode("0103", 0, 5, 1, "Faulkner", "ELEV", "Elevator 3", "Elev3")
+  };
 
   @BeforeEach
   public void emptyDatabase() {
-    System.out.println("Clearing the database!");
-    List<PrototypeNode> listOfNodes = theDatabase.export();
-    for (PrototypeNode n : listOfNodes) {
-      theDatabase.deleteNode(n.getNodeID());
-    }
+    log.debug("Clearing the database!");
+    database.export().forEach((node) -> database.deleteNode(node.getNodeID()));
   }
 
-  // todo delete this method and create real tests
-  @Test
-  public void satisfyCodeCoverage() {
-    PrototypeNode dummyNode = new PrototypeNode("hi", 1, 2, 5, "test", "ELEV", "help", "me");
-    theDatabase.addNode(dummyNode);
+  private void checkEquals(PrototypeNode[] expected, List<PrototypeNode> actual) {
+    assertEquals(new HashSet<PrototypeNode>(Arrays.asList(expected)),
+        new HashSet<PrototypeNode>(actual));
   }
 
   @Test
   public void addSameNodeTest() {
-    PrototypeNode newNode = new PrototypeNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1",
-        "Elev1");
-    theDatabase.addNode(newNode);
-    PrototypeNode sameNodeID = new PrototypeNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1",
-        "Elev1");
-    theDatabase.addNode(sameNodeID);
-    List<PrototypeNode> list = theDatabase.export();
-    assertEquals(1, list.size());
+    database.addNode(testNodes[0]);
+    database.addNode(testNodes[0]);
+    checkEquals(new PrototypeNode[]{testNodes[0]}, database.export());
   }
 
   @Test
   public void addMultipleNodesTest() {
-    PrototypeNode node1 = new PrototypeNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1",
-        "Elev1");
-    PrototypeNode node2 = new PrototypeNode("0102", 9, 0, 1, "Faulkner", "ELEV", "Elevator 2",
-        "Elev2");
-    PrototypeNode node3 = new PrototypeNode("0103", 0, 5, 1, "Faulkner", "ELEV", "Elevator 3",
-        "Elev3");
-    theDatabase.addNode(node1);
-    theDatabase.addNode(node2);
-    theDatabase.addNode(node3);
-    List<PrototypeNode> list = theDatabase.export();
-    assertEquals(3, list.size());
+    database.addNode(testNodes[0]);
+    database.addNode(testNodes[1]);
+    database.addNode(testNodes[2]);
+    checkEquals(testNodes, database.export());
   }
 
   @Test
   public void addAndDeleteNodesTest() {
-    PrototypeNode node1 = new PrototypeNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1",
-        "Elev1");
-    PrototypeNode node2 = new PrototypeNode("0102", 9, 0, 1, "Faulkner", "ELEV", "Elevator 2",
-        "Elev2");
-    PrototypeNode node3 = new PrototypeNode("0103", 0, 5, 1, "Faulkner", "ELEV", "Elevator 3",
-        "Elev3");
-    theDatabase.addNode(node1);
-    theDatabase.addNode(node2);
-    theDatabase.addNode(node3);
-    theDatabase.deleteNode(node2.getNodeID());
-    List<PrototypeNode> list = theDatabase.export();
-    assertEquals(2, list.size());
+    database.addNode(testNodes[0]);
+    database.addNode(testNodes[1]);
+    database.addNode(testNodes[2]);
+    database.deleteNode(testNodes[1].getNodeID());
+    checkEquals(new PrototypeNode[]{testNodes[0], testNodes[2]}, database.export());
   }
 
   @Test
   public void failToDeleteNodesTest() {
-    PrototypeNode node1 = new PrototypeNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1",
-        "Elev1");
-    PrototypeNode node2 = new PrototypeNode("0102", 9, 0, 1, "Faulkner", "ELEV", "Elevator 2",
-        "Elev2");
-    PrototypeNode node3 = new PrototypeNode("0103", 0, 5, 1, "Faulkner", "ELEV", "Elevator 3",
-        "Elev3");
-    theDatabase.addNode(node1);
-    theDatabase.addNode(node2);
-    theDatabase.addNode(node3);
-    theDatabase.deleteNode("0123");
-    List<PrototypeNode> list = theDatabase.export();
-    assertEquals(3, list.size());
+    database.addNode(testNodes[0]);
+    database.addNode(testNodes[1]);
+    database.addNode(testNodes[2]);
+    database.deleteNode("0123");
+    checkEquals(testNodes, database.export());
   }
 
   @Test
   public void addAndUpdateNodeTest() {
-    PrototypeNode node1 = new PrototypeNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1",
-        "Elev1");
-    theDatabase.addNode(node1);
+    database.addNode(testNodes[0]);
     PrototypeNode updatedNode1 = new PrototypeNode("0101", 1, 0, 1, "Faulkner", "ELEV",
         "Elevator 1",
         "Elev1");
-    theDatabase.updateNode(node1.getNodeID(), updatedNode1);
-    List<PrototypeNode> list = theDatabase.export();
-    assertTrue(list.size() == 1 && list.get(0).getXCoord() == 1);
+    database.updateNode(testNodes[0].getNodeID(), updatedNode1);
+    checkEquals(new PrototypeNode[]{updatedNode1}, database.export());
   }
 
   @Test
   public void failUpdateNodeTest() {
-    PrototypeNode node1 = new PrototypeNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1",
-        "Elev1");
-    theDatabase.addNode(node1);
+    database.addNode(testNodes[0]);
     PrototypeNode updatedNode1 = new PrototypeNode("0101", 1, 0, 1, "Faulkner", "ELEV",
         "Elevator 1",
         "Elev1");
-    theDatabase.updateNode("9876", updatedNode1);
-    List<PrototypeNode> list = theDatabase.export();
-    assertTrue(list.size() == 1 && list.get(0).getXCoord() == 0);
+    database.updateNode("9876", updatedNode1);
+    checkEquals(new PrototypeNode[]{testNodes[0]}, database.export());
   }
 
   @Test
   public void addAndUpdateManyTest() {
-    PrototypeNode node1 = new PrototypeNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1",
-        "Elev1");
-    theDatabase.addNode(node1);
+    database.addNode(testNodes[0]);
     PrototypeNode updatedNode1 = new PrototypeNode("0101", 1, 2, 5, "Faulkner", "ELEV",
         "Elevator 1",
         "Elev1");
-    theDatabase.updateNode(node1.getNodeID(), updatedNode1);
-    List<PrototypeNode> list = theDatabase.export();
-    assertTrue(list.size() == 1 && list.get(0).getXCoord() == 1 && list.get(0).getYCoord() == 2
-        && list.get(0).getFloor() == 5);
+    database.updateNode(testNodes[0].getNodeID(), updatedNode1);
+    checkEquals(new PrototypeNode[]{updatedNode1}, database.export());
   }
 
   @Test
   public void exportEmptyTest() {
-    List<PrototypeNode> list = theDatabase.export();
+    List<PrototypeNode> list = database.export();
     assertEquals(0, list.size());
-  }
-
-  @Test
-  public void exportTest() {
-    PrototypeNode node1 = new PrototypeNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1",
-        "Elev1");
-    theDatabase.addNode(node1);
-    PrototypeNode node2 = new PrototypeNode("2", 3, 0, 1, "Faulkner", "ELEV", "Elevator 1",
-        "Elev1");
-    theDatabase.addNode(node2);
-    List<PrototypeNode> list = theDatabase.export();
-    assertTrue(list.size() == 2 && list.contains(node1) && list.contains(node2));
   }
 }
