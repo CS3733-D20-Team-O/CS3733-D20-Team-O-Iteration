13331650c66a640a932648dcfeabd762546b48b7
Benny Klaiman <bnklaiman@wpi.edu>
2020-04-15 21:10:51 -0400

    Fixed the things

--- src/main/java/edu/wpi/onyx_ouroboros/view_model/prototype/ModifyDatabaseViewModel.java
+++ src/main/java/edu/wpi/onyx_ouroboros/view_model/prototype/ModifyDatabaseViewModel.java
@@ -4,16 +4,15 @@ import edu.wpi.onyx_ouroboros.model.DependencyInjector;
 import edu.wpi.onyx_ouroboros.model.data.PrototypeNode;
 import edu.wpi.onyx_ouroboros.model.data.database.DatabaseWrapper;
 import edu.wpi.onyx_ouroboros.view_model.ViewModelBase;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class ModifyDatabaseViewModel extends ViewModelBase {
 
-  Logger logger = Logger.getLogger(ModifyDatabaseViewModel.class.getName());
   @FXML
   private Button addNodeBtn;
   @FXML
@@ -35,9 +34,23 @@ public class ModifyDatabaseViewModel extends ViewModelBase {
   @FXML
   private Button updateNodeBtn;
   @FXML
-  private TextField updateNodeIDField;
+  private TextField updateOldNodeIDField;
   @FXML
-  private TextField updateNodeNodeField;
+  private TextField updateNewNodeIDField;
+  @FXML
+  private TextField updateXCoordField;
+  @FXML
+  private TextField updateYCoordField;
+  @FXML
+  private TextField updateFloorField;
+  @FXML
+  private TextField updateBuildingField;
+  @FXML
+  private TextField updateNodeTypeField;
+  @FXML
+  private TextField updateLongNameField;
+  @FXML
+  private TextField updateShortNameField;
   @FXML
   private Button deleteNodeBtn;
   @FXML
@@ -50,97 +63,117 @@ public class ModifyDatabaseViewModel extends ViewModelBase {
   // Thus, feel free to change the method names and the parameters they take below, but make sure
   // this class provides the appropriate functionality (add, update, & delete)
 
-  // change println to log.(other stuff)
-  // bind button press to fns
-
   @FXML
-  public void addNodeBtn(ActionEvent e) {
+  public void addNodeClicked(ActionEvent e) {
     if (e.getSource() == addNodeBtn) {
-      logger.setLevel(Level.SEVERE);
       if (addIDField.getText().equals("")) {
-        logger.severe(
-            "An error occurred when attempting to add a node.\nThe field must not be empty!");
+        // log not recognized
+        log.error("An error occurred when attempting to add a node.\nThe field must not be empty!");
       } else {
         // haha data parsing go brrrrr
-        CharSequence idCS = addIDField.getCharacters();
-        String nodeID = idCS.toString();
+        String nodeID = addIDField.getText();
 
-        CharSequence xCS, yCS, floorCS;
-        int x, y, floor;
-        xCS = addXCoordField.getCharacters();
-        x = Integer.parseInt(xCS, 0, xCS.length(), 10);
-        yCS = addYCoordField.getCharacters();
-        y = Integer.parseInt(yCS, 0, yCS.length(), 10);
-        floorCS = addFloorField.getCharacters();
-        floor = Integer.parseInt(floorCS, 0, floorCS.length(), 10);
+        String xS = addXCoordField.getText();
+        String yS = addYCoordField.getText();
+        String floorS = addFloorField.getText();
+        int x = Integer.parseInt(xS);
+        int y = Integer.parseInt(yS);
+        int floor = Integer.parseInt(floorS);
 
-        String building = addBuildingField.getCharacters().toString();
-        String nodeType = addNodeTypeField.getCharacters().toString();
-        String longName = addLongNameField.getCharacters().toString();
-        String shortName = addShortNameField.getCharacters().toString();
+        String building = addBuildingField.getText();
+        String nodeType = addNodeTypeField.getText();
+        String longName = addLongNameField.getText();
+        String shortName = addShortNameField.getText();
 
         // fill in the data from text fields
         PrototypeNode aNode = new PrototypeNode(nodeID, x, y, floor, building, nodeType, longName,
             shortName);
-        addNode(aNode);
+        database.addNode(aNode);
       }
     }
   }
 
   @FXML
-  public void addNode(PrototypeNode aNode) {
-    // add node to database
-    database.addNode(aNode);
-  }
-
-  @FXML
-  public void updateNodeBtn(ActionEvent e) {
+  public void updateNodeClicked(ActionEvent e) {
+    String oldNodeID = "";
+    String newNodeID = "";
+    String xS = "";
+    String yS = "";
+    String floorS;
+    String building = "";
+    String nodeType = "";
+    String longName = "";
+    String shortName = "";
+    int x = 9001;
+    int y = 9001;
+    int floor = 2;
     if (e.getSource() == updateNodeBtn) {
-      logger.setLevel(Level.SEVERE);
-      if (updateNodeIDField.getText().equals("") || updateNodeNodeField.getText().equals("")) {
-        logger.severe("An error occurred when attempting to update a node.");
-        if (updateNodeIDField.getText().equals("")) {
-          logger.severe("The ID field must not be empty!");
-        }
-        if (updateNodeNodeField.getText().equals("")) {
-          logger.severe("The node field must not be empty!");
-        }
+      if (updateOldNodeIDField.getText().equals("")) {
+        log.error("An error occurred when attempting to update a node.");
+        log.error("The old ID field must not be empty!");
+      } else if (updateNewNodeIDField.getText().equals("")) {
+        log.error("An error occurred when attempting to update a node.");
+        log.error("The new ID field must not be empty!");
       } else {
         // update node in database
-        String nodeID = updateNodeIDField.getCharacters().toString();
-        PrototypeNode node = new PrototypeNode(nodeID,
-            24, 25, 6,
-            "Sample Building",
-            "Node Type",
-            "Long Name",
-            "Short Name");
-        updateNode(nodeID, node);
+        if (!(updateOldNodeIDField.getText().isBlank())) {
+          oldNodeID = updateOldNodeIDField.getText();
+        }
+
+        if (!(updateNewNodeIDField.getText().isBlank())) {
+          newNodeID = updateNewNodeIDField.getText();
+        }
+
+        if (!(updateXCoordField.getText().isBlank())) {
+          xS = updateXCoordField.getText();
+          x = Integer.parseInt(xS);
+        }
+
+        if (!(updateYCoordField.getText().isBlank())) {
+          yS = updateYCoordField.getText();
+          y = Integer.parseInt(yS);
+        }
+
+        if (!(updateFloorField.getText().isBlank())) {
+          floorS = updateFloorField.getText();
+          floor = Integer.parseInt(floorS);
+        }
+
+        if (!(updateBuildingField.getText().isBlank())) {
+          building = updateBuildingField.getText();
+        }
+
+        if (!(updateNodeTypeField.getText().isBlank())) {
+          nodeType = updateNodeTypeField.getText();
+        }
+
+        if (!(updateLongNameField.getText().isBlank())) {
+          longName = updateLongNameField.getText();
+        }
+
+        if (!(updateShortNameField.getText().isBlank())) {
+          shortName = updateShortNameField.getText();
+        }
+
+        // fill in the data from text fields
+        PrototypeNode newNode = new PrototypeNode(newNodeID, x, y, floor, building, nodeType,
+            longName, shortName);
+        database.updateNode(oldNodeID, newNode);
       }
     }
   }
 
   @FXML
-  public void updateNode(String nodeID, PrototypeNode node) {
-    database.updateNode(nodeID, node);
-  }
-
-  @FXML
-  public void deleteNodeBtn(ActionEvent e) {
+  public void deleteNodeClicked(ActionEvent e) {
     if (e.getSource() == deleteNodeBtn) {
-      logger.setLevel(Level.SEVERE);
       if (deleteNodeIDField.getText().equals("")) {
-        logger.severe(
+        log.error(
             "An error occurred when attempting to delete a node.\nThe field must not be empty!");
       } else {
         // delete node from database
-        String nodeID = deleteNodeIDField.getCharacters().toString();
-        deleteNode(nodeID);
+        String nodeID = deleteNodeIDField.getText();
+        database.deleteNode(nodeID);
       }
     }
-  }
-
-  @FXML
-  public void deleteNode(String nodeID) {
-    database.deleteNode(nodeID);
   }
 }
