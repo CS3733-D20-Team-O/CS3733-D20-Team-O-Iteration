074c7fdfcaf6af94cc2deeed9ad86eb9b539ea94
GregoryConrad <gregorysconrad@gmail.com>
2020-04-16 15:46:44 -0400

    Removed redundant file

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/ServiceRequestSelection.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/ServiceRequestSelection.java
@@ -3,6 +3,7 @@ package edu.wpi.cs3733.d20.teamO.view_model;
 import edu.wpi.cs3733.d20.teamO.model.language.Language;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import lombok.Value;
 
 public class ServiceRequestSelection extends ViewModelBase {
 
@@ -28,5 +29,31 @@ public class ServiceRequestSelection extends ViewModelBase {
    */
   private void styleButtons() {
     // todo style buttons
+  }
+
+  /**
+   * A list of usable service requests
+   * <p>
+   * ALL service requests must be registered here!
+   */
+  public static final Service[] SERVICES = {
+      new Service("serviceGiftDeliveryDescription",
+          "views/service_requests/GiftDeliveryService.fxml"),
+  };
+
+  /**
+   * Represents a usable service
+   */
+  @Value
+  public static class Service {
+
+    /**
+     * The key for the description of this service
+     */
+    String descriptionKey;
+    /**
+     * The fxml file location of the service request user interface
+     */
+    String fxmlFile;
   }
 }
