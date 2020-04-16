c9b70848fb88587fe2e9ffb57861d9c273c56017
GregoryConrad <gregorysconrad@gmail.com>
2020-04-16 17:00:25 -0400

    Started MapView

---
+++ src/main/java/edu/wpi/cs3733/d20/teamO/custom_views/MapView.java
@@ -0,0 +1,93 @@
+package edu.wpi.cs3733.d20.teamO.custom_views;
+
+import edu.wpi.cs3733.d20.teamO.model.data.Node;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Region;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.val;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class MapView extends Region implements Initializable {
+
+  /**
+   * Gets called when a node is clicked
+   */
+  private Consumer<Node> onNodeTappedListener;
+
+  /**
+   * Gets called when a click on the map occurs but it is not on a node
+   */
+  private BiConsumer<Integer, Integer> onMissTapListener;
+
+  /**
+   * The node map to use to fuel the display
+   */
+  private Map<String, Node> nodeMap = new HashMap<>();
+
+  /**
+   * The current floor being displayed
+   */
+  private int floor = 1;
+
+  /**
+   * Called to initialize a controller after its root element has been completely processed.
+   *
+   * @param location  The location used to resolve relative paths for the root object, or {@code
+   *                  null} if the location is not known.
+   * @param resources The resources used to localize the root object, or {@code null} if
+   */
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    // todo draw floor and set up basic stuff
+  }
+
+  public void setNodeMap(Map<String, Node> nodeMap) {
+    this.nodeMap = nodeMap;
+    // Clear so we can redraw the new nodes
+    clear();
+    // Draw all the new nodes
+    drawAll();
+  }
+
+  private void drawAll() {
+    nodeMap.keySet().forEach((id) -> {
+      val node = nodeMap.get(id);
+      if (node.getFloor() == floor) {
+        drawNode(node);
+      }
+    });
+  }
+
+  private void drawNode(Node node) {
+    // todo draw this node
+  }
+
+  private void clear() {
+    // todo clear the canvas
+  }
+
+  public void drawEdge(Node n1, Node n2) {
+    // draw edge between two nodes
+    // in case line goes over nodes, simply call drawNode() for both nodes after drawing line
+  }
+
+  /*
+  image view for floor
+  canvas for nodes and edges
+  listen for an on tap in the canvas
+    - fire either onNodeTappedListener if a node was tapped or the other one
+    - calculate distance from finger to node via Math.hypot
+    - go through the nodeMap and find the closest node (if map isnt empty)
+    - and if the closest node is within the radius + ~10 pixels of each drawn node
+    - call on node tapped, otherwise call the other listener with the coordinates
+  when switch floors clear canvas and redraw
+   */
+}
