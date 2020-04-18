d72882e2e51a8cfb15e6e81ddc91129da4afb599
Mark Hogan <mhogan2@wpi.edu>
2020-04-18 13:01:08 -0400

    issue-99 - pushing current progress. Added a few components.

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/RequestHandlerViewModel.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/RequestHandlerViewModel.java
@@ -1,8 +1,9 @@
 package edu.wpi.cs3733.d20.teamO.view_model.admin;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
 import edu.wpi.cs3733.d20.teamO.model.database.db_model.ServiceRequestProperty;
-import edu.wpi.cs3733.d20.teamO.model.database.db_model.EmployeeProperty;
+//import edu.wpi.cs3733.d20.teamO.model.database.db_model.EmployeeProperty;
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
 import java.util.Date;
 import javafx.fxml.FXML;
@@ -19,23 +20,34 @@ public class RequestHandlerViewModel extends ViewModelBase {
   private TableColumn<ServiceRequestProperty, String> colServiceRequest;
   @FXML
   private TableColumn<Date, String> colDateRequest;
-  @FXML
-  private TableColumn<EmployeeProperty, String> colAssigned;
+//  @FXML
+//  private TableColumn<EmployeeProperty, String> colAssigned;
   @FXML
   private TableView employeeTable;
   @FXML
   private TableColumn<String, String> colEmployeeName;
 
   private JFXButton btnAssign = new JFXButton("Assign");
-
-  @FXML
-  private RadioButton showUnavail;
-
+  private JFXCheckBox cbShowUnavail = new JFXCheckBox("Show Unavailable");
 
   private void setupInterface(){
     btnAssign.getStyleClass().add("button-raised");
-    colServiceRequest.setCellFactory(new PropertyValueFactory<ServiceRequestProperty, String>("Service Requested"));
+    cbShowUnavail.getStyleClass().add("custom-jfx-check-box");
+    //Not certain where this goes
+//    .custom-jfx-check-box{
+//      -jfx-checked-color: #FFB970;
+//      -jfx-unchecked-color: #58A5F0;
+//    }
 
+    PropertyValueFactory colServReq = new PropertyValueFactory("Service Requested");
+    colServiceRequest.setCellFactory(colServReq);
+    PropertyValueFactory colReqDate = new PropertyValueFactory("Date Requested");
+    colServiceRequest.setCellFactory(colReqDate);
+    PropertyValueFactory colEmpAssnd = new PropertyValueFactory("Assigned Employee");
+    colServiceRequest.setCellFactory(colEmpAssnd);
+
+    PropertyValueFactory colEmpName = new PropertyValueFactory("Employee Name");
+    colServiceRequest.setCellFactory(colEmpName);
 
   }
 
