9c87b1b343578d26dc755ce5340d959a9234a771
Mark Hogan <mhogan2@wpi.edu>
2020-04-18 12:48:20 -0400

    issue-99 - pushing current progress.

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/RequestHandlerViewModel.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/RequestHandlerViewModel.java
@@ -1,36 +1,43 @@
 package edu.wpi.cs3733.d20.teamO.view_model.admin;
 
+import com.jfoenix.controls.JFXButton;
+import edu.wpi.cs3733.d20.teamO.model.database.db_model.ServiceRequestProperty;
+import edu.wpi.cs3733.d20.teamO.model.database.db_model.EmployeeProperty;
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
+import java.util.Date;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class RequestHandlerViewModel extends ViewModelBase {
 
   @FXML
   private TableView serviceTable;
   @FXML
-  private TableColumn<String, String> colServiceRequest;
+  private TableColumn<ServiceRequestProperty, String> colServiceRequest;
   @FXML
-  private TableColumn<String, String> colDateRequest;
+  private TableColumn<Date, String> colDateRequest;
   @FXML
-  private TableColumn<String, String> colAssigned;
+  private TableColumn<EmployeeProperty, String> colAssigned;
   @FXML
   private TableView employeeTable;
   @FXML
   private TableColumn<String, String> colEmployeeName;
-  @FXML
-  private Button btnClose;
-  @FXML
-  private Button btnGetEmployees;
-  @FXML
-  private Button btnAssign;
+
+  private JFXButton btnAssign = new JFXButton("Assign");
+
   @FXML
   private RadioButton showUnavail;
 
 
+  private void setupInterface(){
+    btnAssign.getStyleClass().add("button-raised");
+    colServiceRequest.setCellFactory(new PropertyValueFactory<ServiceRequestProperty, String>("Service Requested"));
+
+
+  }
 
 
 }
