e913160253c4acd49dd87b14a22489d5dbc522a9
GregoryConrad <gregorysconrad@gmail.com>
2020-04-18 00:05:23 -0400

    Started same window switching

---
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/NavigationBar.java
@@ -0,0 +1,11 @@
+package edu.wpi.cs3733.d20.teamO.view_model;
+
+import java.net.URL;
+
+public class NavigationBar extends ViewModelBase {
+
+  @Override
+  protected void start(URL location) {
+    super.start(location);
+  }
+}
