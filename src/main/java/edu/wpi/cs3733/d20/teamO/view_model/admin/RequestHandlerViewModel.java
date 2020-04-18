28c942518ed9001c919e8200c957a5cb94b07d5a
Mark Hogan <mhogan2@wpi.edu>
2020-04-18 12:08:29 -0400

    issue-99 - pushing current progress.

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/RequestHandlerViewModel.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/RequestHandlerViewModel.java
@@ -1,7 +1,36 @@
 package edu.wpi.cs3733.d20.teamO.view_model.admin;
 
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class RequestHandlerViewModel extends ViewModelBase {
+
+  @FXML
+  private TableView serviceTable;
+  @FXML
+  private TableColumn<String, String> colServiceRequest;
+  @FXML
+  private TableColumn<String, String> colDateRequest;
+  @FXML
+  private TableColumn<String, String> colAssigned;
+  @FXML
+  private TableView employeeTable;
+  @FXML
+  private TableColumn<String, String> colEmployeeName;
+  @FXML
+  private Button btnClose;
+  @FXML
+  private Button btnGetEmployees;
+  @FXML
+  private Button btnAssign;
+  @FXML
+  private RadioButton showUnavail;
+
+
+
 
 }
