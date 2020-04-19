856c5d9227387af6e0b69aa49599c783800f2326
GregoryConrad <gregorysconrad@gmail.com>
2020-04-19 11:59:12 -0400

    Changed up dialog

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/ServiceRequestSelection.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/ServiceRequestSelection.java
@@ -1,5 +1,6 @@
 package edu.wpi.cs3733.d20.teamO.view_model.kiosk;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXSnackbar;
@@ -7,6 +8,7 @@ import com.jfoenix.controls.JFXSnackbar.SnackbarEvent;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.events.JFXDialogEvent;
 import edu.wpi.cs3733.d20.teamO.events.ForwardNavigationEvent;
+import edu.wpi.cs3733.d20.teamO.model.database.DatabaseWrapper;
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
 import java.net.URL;
 import java.util.HashMap;
@@ -15,6 +17,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
@@ -44,21 +47,30 @@ public class ServiceRequestSelection extends ViewModelBase {
 
   @FXML
   private void onSubmitClicked() {
-//    for (val request : get(DatabaseWrapper.class).exportServiceRequests()) {
-//      if (request.getRequestID().equals(confirmationCode.getText())) {
-    val stackPane = new StackPane();
-    AnchorPane.setTopAnchor(stackPane, 0.0);
-    AnchorPane.setBottomAnchor(stackPane, 0.0);
-    AnchorPane.setLeftAnchor(stackPane, 0.0);
-    AnchorPane.setRightAnchor(stackPane, 0.0);
-    root.getChildren().add(stackPane);
-    JFXDialog dialog = new JFXDialog();
-    dialog.setContent(new Label("Content")); // todo request info
-    dialog.show(stackPane);
-    dialog.addEventHandler(JFXDialogEvent.CLOSED, event -> root.getChildren().remove(stackPane));
-//        return;
-//      }
-//    }
+    for (val request : get(DatabaseWrapper.class).exportServiceRequests()) {
+      if (request.getRequestID().equals(confirmationCode.getText())) {
+        // Create a full-screen stack pane and add it to the window
+        val stackPane = new StackPane();
+        AnchorPane.setTopAnchor(stackPane, 0.0);
+        AnchorPane.setBottomAnchor(stackPane, 0.0);
+        AnchorPane.setLeftAnchor(stackPane, 0.0);
+        AnchorPane.setRightAnchor(stackPane, 0.0);
+        root.getChildren().add(stackPane);
+        // Create and show the dialog
+        JFXDialog dialog = new JFXDialog();
+        val infoText = getString("serviceRequestConfirmationNumber") + ": "
+            + request.getRequestID()
+            + "\n" + getString("serviceRequestAssignedTo") + ": "
+            + request.getEmployeeAssigned();
+        val closeButton = new JFXButton(getString("serviceRequestDialogClose"));
+        closeButton.setOnAction(e -> dialog.close());
+        val container = new VBox(new Label(infoText), closeButton);
+        dialog.setContent(container);
+        dialog.show(stackPane);
+        dialog.addEventHandler(JFXDialogEvent.CLOSED, e -> root.getChildren().remove(stackPane));
+        return;
+      }
+    }
     // If we didn't return in the for loop, that means the request isn't in the database
     JFXSnackbar bar = new JFXSnackbar(root);
     // todo style this snack bar
