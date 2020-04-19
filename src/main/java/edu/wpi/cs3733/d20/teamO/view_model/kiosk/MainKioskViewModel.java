4488c6018685863f4b50a08c77797f86dcb59c8e
GregoryConrad <gregorysconrad@gmail.com>
2020-04-19 15:24:52 -0400

    done except for icon and styling

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/MainKioskViewModel.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/MainKioskViewModel.java
@@ -2,10 +2,12 @@ package edu.wpi.cs3733.d20.teamO.view_model.kiosk;
 
 import com.jfoenix.controls.JFXComboBox;
 import edu.wpi.cs3733.d20.teamO.events.ForwardNavigationEvent;
+import edu.wpi.cs3733.d20.teamO.model.language.LanguageHandler;
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import lombok.val;
 
 public class MainKioskViewModel extends ViewModelBase {
 
@@ -14,8 +16,17 @@ public class MainKioskViewModel extends ViewModelBase {
 
   @Override
   protected void start(URL location, ResourceBundle resources) {
-    // todo set languageSwitcher langs
-    // todo listen to language switcher langs
+    // Set the prompt text to the currently selected language
+    val currentLocale = get(LanguageHandler.class).getCurrentLocale();
+    languageSwitcher.setPromptText(currentLocale.getDisplayName(currentLocale));
+    // Load in the supported locales
+    for (val locale : LanguageHandler.SUPPORTED_LOCALES) {
+      languageSwitcher.getItems().add(locale.getDisplayName(locale));
+    }
+    // Add a listener to switch to the selected locale
+    languageSwitcher.getSelectionModel().selectedIndexProperty().addListener(
+        ((observableValue, oldValue, newValue) -> get(LanguageHandler.class)
+            .setCurrentLocale(LanguageHandler.SUPPORTED_LOCALES[newValue.intValue()])));
   }
 
   @FXML
