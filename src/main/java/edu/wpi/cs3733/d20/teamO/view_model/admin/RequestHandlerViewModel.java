dd73d96f56153e2ff26cb27f15519552b8250c96
Mark Hogan <mhogan2@wpi.edu>
2020-04-18 21:04:46 -0400

    issue-99 - pushing current progress. Added comments and updated the FXML

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/RequestHandlerViewModel.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/RequestHandlerViewModel.java
@@ -2,15 +2,16 @@ package edu.wpi.cs3733.d20.teamO.view_model.admin;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
+import edu.wpi.cs3733.d20.teamO.model.database.db_model.EmployeeProperty;
 import edu.wpi.cs3733.d20.teamO.model.database.db_model.ServiceRequestProperty;
-//import edu.wpi.cs3733.d20.teamO.model.database.db_model.EmployeeProperty;
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
 import java.util.Date;
 import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+
+//import edu.wpi.cs3733.d20.teamO.model.database.db_model.EmployeeProperty;
 
 public class RequestHandlerViewModel extends ViewModelBase {
 
@@ -20,17 +21,16 @@ public class RequestHandlerViewModel extends ViewModelBase {
   private TableColumn<ServiceRequestProperty, String> colServiceRequest;
   @FXML
   private TableColumn<Date, String> colDateRequest;
-//  @FXML
-//  private TableColumn<EmployeeProperty, String> colAssigned;
+  private final JFXButton btnAssign = new JFXButton("Assign");
   @FXML
   private TableView employeeTable;
   @FXML
   private TableColumn<String, String> colEmployeeName;
+  private final JFXCheckBox cbShowUnavail = new JFXCheckBox("Show Unavailable");
+  @FXML
+  private TableColumn<EmployeeProperty, String> colAssigned;
 
-  private JFXButton btnAssign = new JFXButton("Assign");
-  private JFXCheckBox cbShowUnavail = new JFXCheckBox("Show Unavailable");
-
-  private void setupInterface(){
+  private void setupInterface() {
     btnAssign.getStyleClass().add("button-raised");
     cbShowUnavail.getStyleClass().add("custom-jfx-check-box");
     //Not certain where this goes
@@ -49,6 +49,31 @@ public class RequestHandlerViewModel extends ViewModelBase {
     PropertyValueFactory colEmpName = new PropertyValueFactory("Employee Name");
     colServiceRequest.setCellFactory(colEmpName);
 
+  }
+
+  private void updateEmployeeTable() {
+    //when the serviceTable row selected populate the employeeTable
+    //get the serviceTable row serviceRequestProperty.type
+    //populate the employeeTable with employees with the same type
+    //check if employee is available
+    //if not available don't show
+  }
+
+  private void assignEmployee() {
+    //Get current serviceTable row
+    //Get current employeeTable row
+    //Get the employee
+    //update the ServiceRequestProperty for assignedEmployee to the employee
+    //if no employee selected this doesn't work
+//    serviceTable.getItems().get(0);
+
+  }
+
+  private void setCbShowUnavail() {
+    //when box is pressed
+    //if checked=true
+    //set to checked=false
+    //update employeeTable with just service Type
   }
 
 
