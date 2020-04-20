72f3ac7045b046f509c2bf0393483a4698ee4a6d
Victoria Grasso <vygrasso@wpi.edu>
2020-04-20 17:36:43 -0400

    DB testing

--- src/test/java/edu/wpi/cs3733/d20/teamO/model/database/DatabaseWrapperTest.java
+++ src/test/java/edu/wpi/cs3733/d20/teamO/model/database/DatabaseWrapperTest.java
@@ -1,7 +1,15 @@
 package edu.wpi.cs3733.d20.teamO.model.database;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import edu.wpi.cs3733.d20.teamO.model.TestInjector;
+import edu.wpi.cs3733.d20.teamO.model.database.db_model.NodeProperty;
+import edu.wpi.cs3733.d20.teamO.model.database.db_model.Table;
 import edu.wpi.cs3733.d20.teamO.model.datatypes.Node;
+import java.util.HashMap;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Tests DatabaseWrapper
@@ -19,60 +27,86 @@ public class DatabaseWrapperTest {
           "Faulkner", "ELEV", "Elevator 3", "Elev3"),
   };
 
-//  @BeforeEach
-//  public void emptyDatabase() {
-//    database.export().forEach((node) -> database.deleteNode(node.getNodeID()));
-//  }
-//
+  @BeforeEach
+  public void emptyDatabase() {
+    database.deleteFromTable(Table.NODES_TABLE, NodeProperty.NODE_TYPE, "ELEV");
+  }
+
+  //
 //  private void checkResult(Node... expected) {
 //    assertEquals(new HashSet<>(Arrays.asList(expected)), new HashSet<>(database.export()));
 //  }
 //
-//  @Test
-//  public void addSameNodeTest() {
-//    database.addNode(testNodes[0]);
-//    database.addNode(testNodes[0]);
-//    checkResult(testNodes[0]);
-//  }
-//
-//  @Test
-//  public void addMultipleNodesTest() {
-//    Arrays.asList(testNodes).forEach(database::addNode);
-//    checkResult(testNodes);
-//  }
-//
-//  @Test
-//  public void addAndDeleteNodesTest() {
-//    Arrays.asList(testNodes).forEach(database::addNode);
-//    database.deleteNode(testNodes[1].getNodeID());
-//    checkResult(testNodes[0], testNodes[2]);
-//  }
-//
-//  @Test
-//  public void failToDeleteNodesTest() {
-//    Arrays.asList(testNodes).forEach(database::addNode);
-//    database.deleteNode("0123");
-//    checkResult(testNodes);
-//  }
-//
-//  @Test
-//  public void addManyAndUpdateNodeTest() {
-//    Arrays.asList(testNodes).forEach(database::addNode);
-//    val updatedNode = new PrototypeNode("0101", 1, 0, 1,
-//        "Faulkner", "ELEV", "Elevator 1", "Elev1");
-//    database.updateNode(testNodes[0].getNodeID(), updatedNode);
-//    checkResult(updatedNode, testNodes[1], testNodes[2]);
-//  }
-//
-//  @Test
-//  public void failUpdateNodeTest() {
-//    database.addNode(testNodes[0]);
-//    val updatedNode = new PrototypeNode("0101", 1, 0, 1,
-//        "Faulkner", "ELEV", "Elevator 1", "Elev1");
-//    database.updateNode("9876", updatedNode);
-//    checkResult(testNodes[0]);
-//  }
-//
+  @Test
+  public void addSameNodeTest() {
+    database.addNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1", "Elev1");
+    database.addNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1", "Elev1");
+    HashMap map = new HashMap<String, Node>();
+    map.put("0101", testNodes[0]);
+    assertEquals(map, database.exportNodes());
+  }
+
+  @Test
+  public void addMultipleNodesTest() {
+    database.addNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1", "Elev1");
+    database.addNode("0102", 9, 0, 1, "Faulkner", "ELEV", "Elevator 2", "Elev2");
+    database.addNode("0103", 0, 5, 1, "Faulkner", "ELEV", "Elevator 3", "Elev3");
+    HashMap map = new HashMap<String, Node>();
+    map.put("0101", testNodes[0]);
+    map.put("0102", testNodes[1]);
+    map.put("0103", testNodes[2]);
+    assertEquals(map, database.exportNodes());
+  }
+
+  @Test
+  public void addAndDeleteNodesTest() {
+    database.addNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1", "Elev1");
+    database.addNode("0102", 9, 0, 1, "Faulkner", "ELEV", "Elevator 2", "Elev2");
+    database.addNode("0103", 0, 5, 1, "Faulkner", "ELEV", "Elevator 3", "Elev3");
+    database.deleteFromTable(Table.NODES_TABLE, NodeProperty.NODE_ID, "0102");
+    HashMap map = new HashMap<String, Node>();
+    map.put("0101", testNodes[0]);
+    map.put("0103", testNodes[2]);
+    assertEquals(map, database.exportNodes());
+  }
+
+  @Test
+  public void failToDeleteNodesTest() {
+    database.addNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1", "Elev1");
+    database.addNode("0102", 9, 0, 1, "Faulkner", "ELEV", "Elevator 2", "Elev2");
+    database.addNode("0103", 0, 5, 1, "Faulkner", "ELEV", "Elevator 3", "Elev3");
+    database.deleteFromTable(Table.NODES_TABLE, NodeProperty.NODE_ID, "123");
+    HashMap map = new HashMap<String, Node>();
+    map.put("0101", testNodes[0]);
+    map.put("0102", testNodes[1]);
+    map.put("0103", testNodes[2]);
+    assertEquals(map, database.exportNodes());
+  }
+
+  @Test
+  public void addManyAndUpdateNodeTest() {
+    database.addNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1", "Elev1");
+    database.addNode("0102", 9, 0, 1, "Faulkner", "ELEV", "Elevator 2", "Elev2");
+    database.addNode("0103", 0, 5, 1, "Faulkner", "ELEV", "Elevator 3", "Elev3");
+    val updatedNode = new Node("0101", 3, 0, 1, "Faulkner", "ELEV", "Elevator 1", "Elev1");
+    database.update(Table.NODES_TABLE, NodeProperty.NODE_ID, "0101", NodeProperty.X_COORD, 3);
+    HashMap map = new HashMap<String, Node>();
+    map.put("0101", updatedNode);
+    map.put("0102", testNodes[1]);
+    map.put("0103", testNodes[2]);
+    assertEquals(map, database.exportNodes());
+  }
+
+  @Test
+  public void failUpdateNodeTest() {
+    database.addNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1", "Elev1");
+    val updatedNode = new Node("0101", 3, 0, 1, "Faulkner", "ELEV", "Elevator 1", "Elev1");
+    database.update(Table.NODES_TABLE, NodeProperty.NODE_ID, "876", NodeProperty.X_COORD, 3);
+    HashMap map = new HashMap<String, Node>();
+    map.put("0101", testNodes[0]);
+    assertEquals(map, database.exportNodes());
+  }
+
 //  @Test
 //  public void exportEmptyTest() {
 //    checkResult(); // tests empty list with database.export()
