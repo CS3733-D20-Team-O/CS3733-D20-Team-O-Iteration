a2fa4f9d9dee9b13a9e4525ad7b88746a35f1b0f
GregoryConrad <gregorysconrad@gmail.com>
2020-04-20 11:59:06 -0400

    Made the stack-based navigation

--- src/main/java/edu/wpi/cs3733/d20/teamO/Main.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/Main.java
@@ -13,7 +13,6 @@ import edu.wpi.cs3733.d20.teamO.injection_modules.FXMLLoaderModule;
 import edu.wpi.cs3733.d20.teamO.model.database.DatabaseUtilities;
 import edu.wpi.cs3733.d20.teamO.model.datatypes.LoginDetails;
 import edu.wpi.cs3733.d20.teamO.model.language.LanguageHandler;
-import edu.wpi.cs3733.d20.teamO.view_model.NavigationBar;
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
@@ -21,17 +20,9 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.greenrobot.eventbus.EventBus;
@@ -55,11 +46,6 @@ public class Main extends Application {
   );
 
   /**
-   * The root of the entire application
-   */
-  private final StackPane root = new StackPane();
-
-  /**
    * Application entry point
    *
    * @param args the command line arguments that can contain the username and password
@@ -74,20 +60,26 @@ public class Main extends Application {
   @Override
   public void init() {
     log.info("Starting up the application");
+    // Register this application to receive events
     get(EventBus.class).register(this);
+    // Setup the navigator to use the correct main screen fxml by checking login validity
+    val isValid = new LoginDetails(getParameters().getRaw()).isValid();
+    get(Navigator.class).setMainFXML(isValid ? "views/admin/Main.fxml" : "views/kiosk/Main.fxml");
     // Set English as the default language (and trigger the loading of the main screen)
     get(LanguageHandler.class).setCurrentLocale(LanguageHandler.SUPPORTED_LOCALES[0]);
   }
 
   @Override
   public void start(Stage primaryStage) {
-    primaryStage.setScene(new Scene(root));
+    primaryStage.setTitle("Hospital Kiosk Application");
+    primaryStage.setScene(new Scene(get(Navigator.class).getRoot()));
     primaryStage.show();
   }
 
   @Override
   public void stop() throws SQLException {
     log.info("Stopping the application");
+    // Unregister this application from receiving any more events
     get(EventBus.class).unregister(this);
     // Close the database connection
     get(Connection.class).close();
@@ -104,18 +96,6 @@ public class Main extends Application {
     return injector.getInstance(tClass);
   }
 
-      /*
-    todo incorporate this animation code too
-        Timeline timeline = new Timeline();
-        KeyValue kv = new KeyValue(imgView2.translateXProperty(), 0, Interpolator.EASE_BOTH);
-        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
-        timeline.getKeyFrames().add(kf);
-        timeline.setOnFinished(t->{
-            // remove pane and restore scene 1
-            root1.getChildren().setAll(rectangle1);
-        });
-     */
-
   /**
    * Navigates to a new window within the application
    *
@@ -125,87 +105,25 @@ public class Main extends Application {
   @SuppressWarnings("unused") // called by EventBus
   public void onForwardNavigationEvent(ForwardNavigationEvent event) {
     try {
-      // Create the new root for the page
-      val newRoot = new AnchorPane();
-
-      // Set up the navigation bar for the new page
-      val navBarLoader = get(FXMLLoader.class);
-      val navBarRoot = (Region) navBarLoader.load(getClass()
-          .getResourceAsStream("views/NavigationBar.fxml"));
-      ((NavigationBar) navBarLoader.getController()).setTitle(event.getTitle());
-      AnchorPane.setTopAnchor(navBarRoot, 0.0);
-      AnchorPane.setLeftAnchor(navBarRoot, 0.0);
-      AnchorPane.setRightAnchor(navBarRoot, 0.0);
-      newRoot.getChildren().add(navBarRoot);
-
-      // Set up the actual body (content) of the new page
-      val contentLoader = get(FXMLLoader.class);
-      val contentRoot = (Region) contentLoader.load(getClass()
-          .getResourceAsStream(event.getFxmlLocation()));
-      // The height is not available right away, so set top bounds when it is available
-      navBarRoot.heightProperty().addListener(((observable, oldValue, newValue) ->
-          AnchorPane.setTopAnchor(contentRoot, newValue.doubleValue())));
-      AnchorPane.setLeftAnchor(contentRoot, 0.0);
-      AnchorPane.setRightAnchor(contentRoot, 0.0);
-      AnchorPane.setBottomAnchor(contentRoot, 0.0);
-      newRoot.getChildren().add(contentRoot);
-
-      // At this point, newRoot is completely instantiated so execute the transition
-      val currentRoot = root.getChildren().get(0);
-      root.getChildren().add(newRoot);
-      val width = root.getWidth();
-      val start = new KeyFrame(Duration.ZERO,
-          new KeyValue(currentRoot.translateXProperty(), 0),
-          new KeyValue(newRoot.translateXProperty(), width));
-      val end = new KeyFrame(Duration.seconds(0.15),
-          new KeyValue(currentRoot.translateXProperty(), -width),
-          new KeyValue(newRoot.translateXProperty(), 0));
-      val slide = new Timeline(start, end);
-      slide.setOnFinished(e -> root.getChildren().remove(currentRoot));
-      slide.play();
+      get(Navigator.class).push(event.getTitle(), event.getFxmlLocation());
     } catch (IOException e) {
       log.error("Failed to switch to a new window from " + event.getFxmlLocation(), e);
     }
   }
 
   /**
-   * Navigates back to the main screen
+   * Navigates back one screen
    *
    * @param event the event that was fired
-   * @throws IOException in case the main screen cannot be loaded
    */
   @Subscribe
   @SuppressWarnings("unused") // called by EventBus
-  public void onBackwardNavigationEvent(BackwardNavigationEvent event) throws IOException {
-    // Get a copy of the current root (it will be the only child of the actual root)
-    val currentRoot = root.getChildren().get(0);
-    // Add the main screen back to the root to get ready for the slide transition
-    loadMainScreen();
-    val mainScreenRoot = root.getChildren().get(1);
-    // Start the slide animation
-    val width = root.getWidth();
-    val start = new KeyFrame(Duration.ZERO,
-        new KeyValue(mainScreenRoot.translateXProperty(), -width),
-        new KeyValue(currentRoot.translateXProperty(), 0));
-    val end = new KeyFrame(Duration.seconds(0.15),
-        new KeyValue(mainScreenRoot.translateXProperty(), 0),
-        new KeyValue(currentRoot.translateXProperty(), width));
-    val slide = new Timeline(start, end);
-    slide.setOnFinished(e -> root.getChildren().remove(currentRoot));
-    slide.play();
-  }
-
-  /**
-   * Loads the main application screen into the root
-   * <p>
-   * TODO Cleanup this method and make more efficient (cache login details in init probably)
-   *
-   * @throws IOException in case the main screen cannot be loaded
-   */
-  private void loadMainScreen() throws IOException {
-    val isLoginValid = new LoginDetails(getParameters().getRaw()).isValid();
-    val mainFxml = isLoginValid ? "views/admin/Main.fxml" : "views/kiosk/Main.fxml";
-    root.getChildren().add(get(FXMLLoader.class).load(getClass().getResourceAsStream(mainFxml)));
+  public void onBackwardNavigationEvent(BackwardNavigationEvent event) {
+    try {
+      get(Navigator.class).pop();
+    } catch (IOException e) {
+      log.error("Failed to go backwards in a backward navigation event", e);
+    }
   }
 
   /**
@@ -216,9 +134,8 @@ public class Main extends Application {
   @Subscribe
   @SuppressWarnings("unused") // called by EventBus
   public void onLanguageSwitch(LanguageSwitchEvent event) {
-    root.getChildren().clear();
     try {
-      loadMainScreen();
+      get(Navigator.class).empty();
     } catch (IOException e) {
       log.error("Failed to reload the main screen on a language switch");
     }
