9e279d4232c245cef84a085d676d00bbedc06351
Victoria Grasso <vygrasso@wpi.edu>
2020-04-21 01:05:54 -0400

    DB tests

--- src/test/java/edu/wpi/cs3733/d20/teamO/model/database/DatabaseWrapperTest.java
+++ src/test/java/edu/wpi/cs3733/d20/teamO/model/database/DatabaseWrapperTest.java
@@ -5,8 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import edu.wpi.cs3733.d20.teamO.model.TestInjector;
 import edu.wpi.cs3733.d20.teamO.model.database.db_model.NodeProperty;
 import edu.wpi.cs3733.d20.teamO.model.database.db_model.Table;
+import edu.wpi.cs3733.d20.teamO.model.datatypes.Edge;
 import edu.wpi.cs3733.d20.teamO.model.datatypes.Node;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 
@@ -26,11 +29,14 @@ public class DatabaseWrapperTest {
           "Faulkner", "ELEV", "Elevator 3", "Elev3"),
   };
 
-  //
-//  private void checkResult(Node... expected) {
-//    assertEquals(new HashSet<>(Arrays.asList(expected)), new HashSet<>(database.export()));
-//  }
-//
+  private final Edge[] testEdges = {
+      new Edge("01", "0101", "0102"),
+  };
+
+  private void checkResultEdges(Edge... expected) {
+    assertEquals(new HashSet<>(Arrays.asList(expected)), new HashSet<>(database.exportEdges()));
+  }
+
   @Test
   public void addSameNodeTest() {
     database.addNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1", "Elev1");
@@ -101,10 +107,19 @@ public class DatabaseWrapperTest {
     assertEquals(map, database.exportNodes());
   }
 
-//  @Test
-//  public void exportEmptyTest() {
-//    checkResult(); // tests empty list with database.export()
-//  }
+  @Test
+  public void exportEmptyTest() {
+    HashMap map = new HashMap<String, Node>(); // tests empty list with database.export()
+    assertEquals(map, database.exportNodes());
+  }
+
+  @Test
+  public void addEdgeTest() {
+    database.addNode("0101", 0, 0, 1, "Faulkner", "ELEV", "Elevator 1", "Elev1");
+    database.addNode("0102", 9, 0, 1, "Faulkner", "ELEV", "Elevator 2", "Elev2");
+    database.addEdge("01", "0101", "0102");
+    checkResultEdges(testEdges);
+  }
 
   @Test
   public void convertEmployeeIDtoNameTest() {
