af400eab03db0c8c805952263cdbee7c5ba69d7d
Benny Klaiman <bnklaiman@wpi.edu>
2020-04-21 19:26:19 -0400

    pushed things

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/GiftDeliveryService.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/GiftDeliveryService.java
@@ -9,11 +9,14 @@ import edu.wpi.cs3733.d20.teamO.model.datatypes.Node;
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Map;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javax.inject.Inject;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 
 @Slf4j
 @RequiredArgsConstructor(onConstructor_ = {@Inject})
@@ -82,34 +85,36 @@ public class GiftDeliveryService extends ViewModelBase {
         firstNameField) == checkField(middleNameField) == checkField(lastNameField) == checkField(
         emailAddressField)) {
       //send all the text fields to be verified and processed
-      String to = toField.toString();
-      String from = fromField.toString();
-      String ccNumberString = ccNumberField.toString();
-      long ccNumber = Long.parseLong(ccNumberString);
-      String ccMonthString = ccMonthField.toString();
-      int ccYear = Integer.parseInt(ccMonthString);
-      String ccYearString = ccYearField.toString();
-      int ccMonth = Integer.parseInt(ccYearString);
-      String ccSecurityString = ccSecurityField.toString();
-      int ccSecurity = Integer.parseInt(ccSecurityString);
-      String firstName = firstNameField.toString();
-      String middleName = middleNameField.toString();
-      String lastName = lastNameField.toString();
-      String emailAddress = emailAddressField.toString();
+      val to = toField.toString();
+      val from = fromField.toString();
+      val ccNumberString = ccNumberField.toString();
+      val ccNumber = Long.parseLong(ccNumberString);
+      val ccMonthString = ccMonthField.toString();
+      val ccYear = Integer.parseInt(ccMonthString);
+      val ccYearString = ccYearField.toString();
+      val ccMonth = Integer.parseInt(ccYearString);
+      val ccSecurityString = ccSecurityField.toString();
+      val ccSecurity = Integer.parseInt(ccSecurityString);
+      val firstName = firstNameField.toString();
+      val middleName = middleNameField.toString();
+      val lastName = lastNameField.toString();
+      val emailAddress = emailAddressField.toString();
 
       if (verifyCCInfo(ccNumber, ccYear, ccMonth, ccSecurity)) {
         //once everything is correct, save to DB
-        // val database = get(DatabaseWrapper.class);
-        // FIXME
 
-        // val newRequestID = "1";//this needs to be something different.
-        // val requestRoomNode = inRoomComboBox.getItems().add() // on the right track, just need to figure out what to do here
+        val newRequestID = generateRequestID();  //this needs to be something different.
+        val requestFloorNode = onFloorComboBox.getItems().addAll();
+        val requestRoomNode = inRoomComboBox.getItems().add(database
+            .exportServiceRequests(); // on the right track, just need to figure out what to do here
 
         // this if statement will eventually be removed
         // boolean test = true;
         // if (!test) {
         // String requestID, String requestTime, requestNode, type, requesterName, whoMarked, employeeAssigned;
-        // database.addServiceRequest(newRequestID, getTime(), requestRoomNode, "Gift", toField.getText(), "", "");
+        database
+            .addServiceRequest(newRequestID, getTime(), requestRoomNode, "Gift", toField.getText(),
+                "", "");
         // }
         // FIXME
 
@@ -122,6 +127,31 @@ public class GiftDeliveryService extends ViewModelBase {
       log.error("Please fill out all fields.");
     }
   }
+
+  private ArrayList<String> getAvailableFloors() {
+    ArrayList<String> listOfFloors = new ArrayList<String>();
+    for (Map.Entry<String, Node> node : database.exportNodes().entrySet()) {
+      int floorNum = node.getValue().getFloor();
+      String tempString = Integer.toString(floorNum);
+      if (listOfFloors.contains(node.getKey())) {
+
+      }
+    }
+    database.exportNodes().values().toArray();
+    return listOfFloors;
+  }
+
+  private ArrayList<String> getAvailableRooms() {
+    node.getValue().getFloor()
+  }
+
+  private String generateRequestID() {
+    val currentRequestID = database.exportServiceRequests()
+        .get(database.exportServiceRequests().size() - 1).getRequestID();
+    val currentIDInt = Integer.parseInt(currentRequestID) + 1;
+    return Integer.toString(currentIDInt);
+  }
+
 
   private boolean checkField(JFXTextField tf) {
     //take in a field, check the text so it's not empty
