741eaf144e7c94f2574b13c82c7d145bdefdb4e2
GregoryConrad <gregorysconrad@gmail.com>
2020-04-21 23:56:09 -0400

    added snackbar to csv import & export

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/ImportExportCSVViewModel.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/ImportExportCSVViewModel.java
@@ -1,11 +1,18 @@
 package edu.wpi.cs3733.d20.teamO.view_model.admin;
 
 import com.google.inject.Inject;
+import com.jfoenix.controls.JFXSnackbar;
+import com.jfoenix.controls.JFXSnackbar.SnackbarEvent;
 import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733.d20.teamO.model.csv.CSVHandler;
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 
 @RequiredArgsConstructor(onConstructor_ = {@Inject})
 public class ImportExportCSVViewModel extends ViewModelBase {
@@ -13,45 +20,61 @@ public class ImportExportCSVViewModel extends ViewModelBase {
   private final CSVHandler csvHandler;
 
   @FXML
+  private AnchorPane root;
+
+  @FXML
   private JFXTextField nodesFilename, edgesFilename, employeesFilename, requestsFilename;
+
+  private void showConfirmation(boolean didWork) {
+    JFXSnackbar bar = new JFXSnackbar(root);
+    val label = new Label(didWork ?
+        "Operation completed successfully" :
+        "Operation failed. Invalid filename?");
+    label.setStyle("-fx-text-fill: floralwhite");
+    val container = new HBox(label);
+    // Add 16 margin and 16 padding as per material design guidelines
+    container.setStyle("-fx-background-color: #323232;  -fx-background-insets: 16");
+    container.setPadding(new Insets(32)); // total padding, including margin
+    bar.enqueue(new SnackbarEvent(container));
+  }
 
   @FXML
   private void importNodes() {
-    csvHandler.importNodes(nodesFilename.getText());
+    showConfirmation(csvHandler.importNodes(nodesFilename.getText()));
   }
 
   @FXML
   private void exportNodes() {
-    csvHandler.exportNodes(nodesFilename.getText());
+    showConfirmation(csvHandler.exportNodes(nodesFilename.getText()));
   }
 
   @FXML
   private void importEdges() {
-    csvHandler.importEdges(edgesFilename.getText());
+    showConfirmation(csvHandler.importEdges(edgesFilename.getText()));
   }
 
   @FXML
   private void exportEdges() {
-    csvHandler.exportEdges(edgesFilename.getText());
+    showConfirmation(csvHandler.exportEdges(edgesFilename.getText()));
   }
 
   @FXML
   private void importEmployees() {
-    csvHandler.importEmployees(employeesFilename.getText());
+    showConfirmation(csvHandler.importEmployees(employeesFilename.getText()));
   }
 
   @FXML
   private void exportEmployees() {
-    csvHandler.exportEmployees(employeesFilename.getText());
+    showConfirmation(csvHandler.exportEmployees(employeesFilename.getText()));
   }
 
   @FXML
   private void importRequests() {
-    csvHandler.importServiceRequests(requestsFilename.getText());
+    showConfirmation(csvHandler.importServiceRequests(requestsFilename.getText()));
   }
 
   @FXML
   private void exportRequests() {
-    csvHandler.exportServiceRequests(requestsFilename.getText());
+    showConfirmation(csvHandler.exportServiceRequests(requestsFilename.getText()));
   }
 }
