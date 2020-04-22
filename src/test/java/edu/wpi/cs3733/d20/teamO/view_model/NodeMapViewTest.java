fc10e7fd1f2bfeefe2ef2ac094763ca680223643
GregoryConrad <gregorysconrad@gmail.com>
2020-04-21 23:06:18 -0400

    Fixes changed files

--- src/test/java/edu/wpi/cs3733/d20/teamO/view_model/NodeMapViewTest.java
+++ src/test/java/edu/wpi/cs3733/d20/teamO/view_model/NodeMapViewTest.java
@@ -1,17 +1,15 @@
 package edu.wpi.cs3733.d20.teamO.view_model;
 
-import com.google.common.eventbus.EventBus;
 import edu.wpi.cs3733.d20.teamO.Main;
 import edu.wpi.cs3733.d20.teamO.model.datatypes.Node;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import lombok.val;
+import org.greenrobot.eventbus.EventBus;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -29,10 +27,6 @@ public class NodeMapViewTest extends FxRobot {
 
   @Mock
   EventBus eventBus;
-  @Mock
-  Consumer<Node> onNodeTappedListener;
-  @Mock
-  BiConsumer<Integer, Integer> onMissTapListener;
 
   @InjectMocks
   NodeMapView viewModel;
@@ -99,5 +93,32 @@ public class NodeMapViewTest extends FxRobot {
     viewModel.drawEdge(nodeMap.get("Node5"), nodeMap.get("Node6"));
     viewModel.incrementFloor();
     viewModel.drawEdge(nodeMap.get("Node5"), nodeMap.get("Node6"));
+  }
+
+  @Test
+  void testNodeMapAddDeleteNode() {
+    Node node1 = new Node("Node1", 100, 100, 1, "Test", "Test", "Test", "Test");
+    Node node2 = new Node("Node2", 100, 200, 1, "Test", "Test", "Test", "Test");
+    Node node3 = new Node("Node3", 200, 100, 1, "Test", "Test", "Test", "Test");
+    Node node4 = new Node("Node4", 200, 200, 1, "Test", "Test", "Test", "Test");
+    Node node5 = new Node("Node5", 100, 100, 2, "Test", "Test", "Test", "Test");
+    Node node6 = new Node("Node6", 200, 200, 2, "Test", "Test", "Test", "Test");
+    Node node7 = new Node("Node6", 200, 200, 2, "Test", "Test", "Test", "Test");
+    viewModel.addNode(node1);
+    viewModel.addNode(node2);
+    viewModel.addNode(node3);
+    viewModel.addNode(node4);
+    viewModel.addNode(node5);
+    viewModel.addNode(node6);
+    viewModel.incrementFloor();
+    viewModel.decrementFloor();
+    viewModel.deleteNode(node5);
+    viewModel.deleteNode(node1);
+    viewModel.deleteNode(node2);
+    viewModel.deleteNode(node3);
+    viewModel.deleteNode(node4);
+    viewModel.incrementFloor();
+    viewModel.deleteNode(node6);
+    viewModel.deleteNode(node7);
   }
 }
