892d0ba2c60848b2db3046ecb6d467a8066821d4
GregoryConrad <gregorysconrad@gmail.com>
2020-04-22 12:20:26 -0400

    Fixes jar creation

---
+++ src/main/java/edu/wpi/cs3733/d20/teamO/Starter.java
@@ -0,0 +1,11 @@
+package edu.wpi.cs3733.d20.teamO;
+
+/**
+ * Serves as a temporary entry point to the application in order to build a jar
+ */
+public class Starter {
+
+  public static void main(String[] args) {
+    Main.main(args);
+  }
+}
