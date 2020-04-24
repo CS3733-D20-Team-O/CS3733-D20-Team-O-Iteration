21a03e90fb675c24e1e034187496f65e8d7ca7dc
GregoryConrad <gregorysconrad@gmail.com>
2020-04-24 16:59:16 -0400

    Fixed up the import/export screen

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/ImportExportCSVViewModel.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/ImportExportCSVViewModel.java
@@ -1,17 +1,30 @@
 package edu.wpi.cs3733.d20.teamO.view_model.admin;
 
 import com.google.inject.Inject;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXSnackbar;
 import com.jfoenix.controls.JFXSnackbar.SnackbarEvent;
-import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.effects.JFXDepthManager;
 import edu.wpi.cs3733.d20.teamO.model.csv.CSVHandler;
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.function.Function;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.val;
 
 @RequiredArgsConstructor(onConstructor_ = {@Inject})
@@ -19,62 +32,100 @@ public class ImportExportCSVViewModel extends ViewModelBase {
 
   private final CSVHandler csvHandler;
 
-  @FXML
-  private AnchorPane root;
+  private final SimpleStringProperty fileLocation = new SimpleStringProperty("");
+
+  private List<DataType> dataTypes;
+  private DataType currentDataType;
 
   @FXML
-  private JFXTextField nodesFilename, edgesFilename, employeesFilename, requestsFilename;
+  private StackPane root;
+  @FXML
+  private VBox container;
+  @FXML
+  private JFXComboBox<String> typeSelector;
+  @FXML
+  private JFXButton importButton, exportButton;
 
-  private void showConfirmation(boolean didWork) {
+  @Override
+  protected void start(URL location, ResourceBundle resources) {
+    // Add elevation to container
+    JFXDepthManager.setDepth(container, 3);
+
+    // Set the data types
+    dataTypes = new ArrayList<>(Arrays.asList(
+        new DataType("Nodes", csvHandler::importNodes, csvHandler::exportNodes),
+        new DataType("Edges", csvHandler::importEdges, csvHandler::exportEdges),
+        new DataType("Employees", csvHandler::importEmployees, csvHandler::exportEmployees),
+        new DataType("Service Requests",
+            csvHandler::importServiceRequests, csvHandler::exportServiceRequests)));
+    // Add all the data types to the selection box
+    dataTypes.forEach(dataType -> typeSelector.getItems().add(dataType.name));
+    // Add a listener to the selection box
+    typeSelector.getSelectionModel().selectedIndexProperty().addListener(
+        (observable, oldNum, newNum) -> currentDataType = dataTypes.get(newNum.intValue()));
+    // Pre-select the first option
+    typeSelector.getSelectionModel().select(0);
+  }
+
+  private void showSnackBar(String text) {
     JFXSnackbar bar = new JFXSnackbar(root);
-    val label = new Label(didWork ?
-        "Operation completed successfully" :
-        "Operation failed. Invalid filename?");
+    val label = new Label(text);
     label.setStyle("-fx-text-fill: floralwhite");
     val container = new HBox(label);
-    // Add 16 margin and 16 padding as per material design guidelines
     container.setStyle("-fx-background-color: #323232;  -fx-background-insets: 16");
     container.setPadding(new Insets(32)); // total padding, including margin
     bar.enqueue(new SnackbarEvent(container));
   }
 
-  @FXML
-  private void importNodes() {
-    showConfirmation(csvHandler.importNodes(nodesFilename.getText()));
+  private void showConfirmation(boolean didWork) {
+    showSnackBar(didWork ? "Operation completed successfully" : "Operation failed. Invalid file?");
   }
 
   @FXML
-  private void exportNodes() {
-    showConfirmation(csvHandler.exportNodes(nodesFilename.getText()));
+  private void selectFile() {
+    val fileChooser = new FileChooser();
+    fileChooser.setTitle("Open CSV Data File");
+    fileChooser.getExtensionFilters()
+        .addAll(new ExtensionFilter("CSV Files", "*.csv"));
+    val selectedFile = fileChooser.showOpenDialog(root.getScene().getWindow());
+    if (selectedFile != null) {
+      importButton.setDisable(false);
+      exportButton.setDisable(false);
+      setFileLocation(selectedFile.getAbsolutePath());
+    }
   }
 
   @FXML
-  private void importEdges() {
-    showConfirmation(csvHandler.importEdges(edgesFilename.getText()));
+  private void importData() {
+    showConfirmation(currentDataType.importData.apply(getFileLocation()));
   }
 
   @FXML
-  private void exportEdges() {
-    showConfirmation(csvHandler.exportEdges(edgesFilename.getText()));
+  private void exportData() {
+    showConfirmation(currentDataType.exportData.apply(getFileLocation()));
   }
 
-  @FXML
-  private void importEmployees() {
-    showConfirmation(csvHandler.importEmployees(employeesFilename.getText()));
+  public String getFileLocation() {
+    return fileLocation.get();
   }
 
-  @FXML
-  private void exportEmployees() {
-    showConfirmation(csvHandler.exportEmployees(employeesFilename.getText()));
+  public SimpleStringProperty fileLocationProperty() {
+    return fileLocation;
   }
 
-  @FXML
-  private void importRequests() {
-    showConfirmation(csvHandler.importServiceRequests(requestsFilename.getText()));
+  public void setFileLocation(String fileLocation) {
+    this.fileLocation.set(fileLocation);
   }
 
-  @FXML
-  private void exportRequests() {
-    showConfirmation(csvHandler.exportServiceRequests(requestsFilename.getText()));
+  /**
+   * Represents a data type that can be imported/exported from the database
+   * <p>
+   * (Like nodes, edges, employees, and service requests)
+   */
+  @Value
+  private static class DataType {
+
+    String name;
+    Function<String, Boolean> importData, exportData;
   }
 }
