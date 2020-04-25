9c529c7aba337fa33155e7adce5b4304abe5f295
GregoryConrad <gregorysconrad@gmail.com>
2020-04-24 20:59:04 -0400

    Cleaned up the code and added material wrapper classes

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/ImportExportCSVViewModel.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/ImportExportCSVViewModel.java
@@ -3,10 +3,9 @@ package edu.wpi.cs3733.d20.teamO.view_model.admin;
 import com.google.inject.Inject;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.JFXSnackbar;
-import com.jfoenix.controls.JFXSnackbar.SnackbarEvent;
-import com.jfoenix.effects.JFXDepthManager;
+import com.jfoenix.controls.JFXDialog;
 import edu.wpi.cs3733.d20.teamO.model.csv.CSVHandler;
+import edu.wpi.cs3733.d20.teamO.model.material.SnackBar;
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
 import java.net.URL;
 import java.util.ArrayList;
@@ -16,10 +15,6 @@ import java.util.ResourceBundle;
 import java.util.function.Function;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
@@ -31,6 +26,7 @@ import lombok.val;
 public class ImportExportCSVViewModel extends ViewModelBase {
 
   private final CSVHandler csvHandler;
+  private final SnackBar snackBar;
   private final FileChooser fileChooser;
 
   private final SimpleStringProperty fileLocation = new SimpleStringProperty("");
@@ -39,19 +35,14 @@ public class ImportExportCSVViewModel extends ViewModelBase {
   private DataType currentDataType;
 
   @FXML
-  private StackPane root;
-  @FXML
-  private VBox container;
+  private VBox root;
   @FXML
   private JFXComboBox<String> typeSelector;
   @FXML
-  private JFXButton importButton, exportButton;
+  private JFXButton importButton, exportButton, closeButton;
 
   @Override
   protected void start(URL location, ResourceBundle resources) {
-    // Add elevation to container
-    JFXDepthManager.setDepth(container, 3);
-
     // Set the data types
     dataTypes = new ArrayList<>(Arrays.asList(
         new DataType("Nodes", csvHandler::importNodes, csvHandler::exportNodes),
@@ -66,20 +57,6 @@ public class ImportExportCSVViewModel extends ViewModelBase {
         (observable, oldNum, newNum) -> currentDataType = dataTypes.get(newNum.intValue()));
     // Pre-select the first option
     typeSelector.getSelectionModel().select(0);
-  }
-
-  private void showSnackBar(String text) {
-    JFXSnackbar bar = new JFXSnackbar(root);
-    val label = new Label(text);
-    label.setStyle("-fx-text-fill: floralwhite");
-    val container = new HBox(label);
-    container.setStyle("-fx-background-color: #323232;  -fx-background-insets: 16");
-    container.setPadding(new Insets(32)); // total padding, including margin
-    bar.enqueue(new SnackbarEvent(container));
-  }
-
-  private void showConfirmation(boolean didWork) {
-    showSnackBar(didWork ? "Operation completed successfully" : "Operation failed. Invalid file?");
   }
 
   @FXML
@@ -97,12 +74,27 @@ public class ImportExportCSVViewModel extends ViewModelBase {
 
   @FXML
   private void importData() {
-    showConfirmation(currentDataType.importData.apply(getFileLocation()));
+    if (currentDataType.importData.apply(getFileLocation())) {
+      snackBar.show("Successfully imported the " + currentDataType.name.toLowerCase());
+    } else {
+      snackBar.show("Failed to import the " + currentDataType.name.toLowerCase());
+    }
   }
 
   @FXML
   private void exportData() {
-    showConfirmation(currentDataType.exportData.apply(getFileLocation()));
+    if (currentDataType.exportData.apply(getFileLocation())) {
+      snackBar.show("Successfully exported the " + currentDataType.name.toLowerCase());
+    } else {
+      snackBar.show("Failed to export the " + currentDataType.name.toLowerCase());
+    }
+  }
+
+  /**
+   * @param dialog the dialog that owns this view model (and will be closed by the close button)
+   */
+  public void setDialog(JFXDialog dialog) {
+    closeButton.setOnAction(e -> dialog.close());
   }
 
   public String getFileLocation() {
