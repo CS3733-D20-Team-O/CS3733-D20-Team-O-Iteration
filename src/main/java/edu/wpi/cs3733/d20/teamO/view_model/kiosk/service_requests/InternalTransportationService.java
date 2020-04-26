343852897816b39b55ac19927ff6cae25898f71b
jacasella <casellaja@gmail.com>
2020-04-26 11:37:22 -0400

    Everything but tests should be done

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/InternalTransportationService.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/InternalTransportationService.java
@@ -7,9 +7,12 @@ import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.base.IFXValidatableControl;
 import edu.wpi.cs3733.d20.teamO.model.database.DatabaseWrapper;
+import edu.wpi.cs3733.d20.teamO.model.datatypes.Node;
 import edu.wpi.cs3733.d20.teamO.model.datatypes.requests_data.InternalTransportationRequestData;
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.stream.Stream;
 import javafx.fxml.FXML;
@@ -18,6 +21,7 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.ToggleGroup;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 
 @RequiredArgsConstructor(onConstructor_ = {@Inject})
 public class InternalTransportationService extends ViewModelBase {
@@ -25,9 +29,9 @@ public class InternalTransportationService extends ViewModelBase {
   private final DatabaseWrapper database;
 
   @FXML
-  private JFXComboBox currentFloor;
+  private JFXComboBox<String> currentFloor;
   @FXML
-  private JFXComboBox currentRoom;
+  private JFXComboBox<String> currentRoom;
   @FXML
   private JFXTextField reqName;
   @FXML
@@ -45,37 +49,120 @@ public class InternalTransportationService extends ViewModelBase {
   @FXML
   private JFXRadioButton wheelchair, bed, gurney, escort;
   @FXML
-  private JFXComboBox destinationFloor, destinationRoom;
+  private JFXComboBox<String> destinationFloor, destinationRoom;
 
   //assisted
   @FXML
   private JFXRadioButton crutches, independentWheelchair, legScooter, mobileIV;
 
 
+  private Node entryNode;
+
   @Override
   protected void start(URL location, ResourceBundle resources) {
-    //todo initialize the drop downs
-  }
+    addAvailableFloors();
 
+  }
 
   @FXML
   private void onSubmit() {
     InternalTransportationRequestData data;
 
-    boolean test = Stream.of(currentRoom).allMatch(IFXValidatableControl::validate);
 
     //select which fields to validate
-    if (assistedTransportation.isSelected() /* && validate appropriate fields*/) {
+    if (assistedTransportation.isSelected() && validateAssisted()) {
       data = new InternalTransportationRequestData("Assisted",
           ((RadioButton) allRadio.getSelectedToggle()).getText(),
-          destinationFloor.getSelectionModel().getSelectedItem().toString());
-    } else if (unassistedTransportation.isSelected() /* && validate appropriate fields*/) {
+          destinationFloor.getSelectionModel().getSelectedItem());
+    } else if (unassistedTransportation.isSelected() && validateUnassisted()) {
       data = new InternalTransportationRequestData("Unassisted",
           ((RadioButton) allRadio.getSelectedToggle()).getText(), "None required");
     } else {
-      //snack bar error
-      return;
+      return; //cancel for if no data was created;
     }
-    //make database entry
+    //todo make it so data happens
+    database.addServiceRequest("TEMPID", reqTime.getText(), entryNode.getNodeID(), "Int. Transport",
+        reqName.getText(), "", "");
+  }
+
+
+  private boolean validateAssisted() {
+    return (allRadio.getSelectedToggle() != null) &&
+        Stream.of(currentRoom, reqName, reqTime, destinationRoom)
+            .allMatch(IFXValidatableControl::validate);
+  }
+
+  private boolean validateUnassisted() {
+    return (allRadio.getSelectedToggle() != null) &&
+        Stream.of(currentRoom, reqTime)
+            .allMatch(IFXValidatableControl::validate);
+  }
+
+  private void addAvailableFloors() {
+    currentFloor.getItems().addAll("1", "2", "3", "4", "5");
+    destinationFloor.getItems().addAll("1", "2", "3", "4", "5");
+    currentFloor.getSelectionModel().selectFirst();
+    destinationFloor.getSelectionModel().selectFirst();
+    doCurrentFloor();
+    doDestinationFloor();
+  }
+
+  /**
+   * function to display appropriate rooms when a floor is selected in current floor
+   */
+  @FXML
+  private void doCurrentFloor() {
+    doFloor(currentRoom, currentFloor.getSelectionModel().getSelectedItem());
+  }
+
+  /**
+   * function to display appropriate rooms when a floor is selected in destination floor
+   */
+  @FXML
+  private void doDestinationFloor() {
+    doFloor(destinationRoom, destinationFloor.getSelectionModel().getSelectedItem());
+  }
+
+  /**
+   * takes a combobox of floors and populates the corresponding room combo box with appropriate
+   * values
+   *
+   * @param destinationRoom the combobox to populate
+   * @param floor           the floor selected
+   */
+  private void doFloor(JFXComboBox destinationRoom, String floor) {
+    destinationRoom.getItems().clear();
+    for (Map.Entry<String, Node> node : database.exportNodes().entrySet()) {
+      val roomToAdd = node.getValue().getLongName();
+      // roomNode = node.getValue();
+      val listOfRooms = new LinkedList<String>();
+      if (/*!listOfRooms.contains(roomToAdd) &&*/
+          !node.getValue().getNodeType().equals("STAI") &&
+              !node.getValue().getNodeType().equals("ELEV") &&
+              !node.getValue().getNodeType().equals("REST") &&
+              !node.getValue().getNodeType().equals("HALL") &&
+              node.getValue().getFloor() == Integer.parseInt(floor)) {
+        //listOfRooms.add(roomNode);
+        destinationRoom.getItems().add(roomToAdd);
+      }
+    }
+  }
+
+  /**
+   * clears the radio buttons when the tab is switched
+   */
+  @FXML
+  private void switchTabs() {
+    allRadio.selectToggle(null);
+  }
+
+  @FXML
+  private void setNode() {
+    for (Map.Entry<String, Node> n : database.exportNodes().entrySet()) {
+      if (n.getValue().getLongName().equals(currentFloor.getSelectionModel().getSelectedItem())) {
+        entryNode = n.getValue();
+        break;
+      }
+    }
   }
 }
