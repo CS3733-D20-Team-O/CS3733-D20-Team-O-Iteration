fbd9dd86374510f7fbc556b4045e8b35fc8490f8
GregoryConrad <gregorysconrad@gmail.com>
2020-04-26 15:16:22 -0400

    Waiting for material wrappers to be merged into master

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/SanitationService.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/SanitationService.java
@@ -2,23 +2,19 @@ package edu.wpi.cs3733.d20.teamO.view_model.kiosk.service_requests;
 
 import com.google.inject.Inject;
 import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
-import com.jfoenix.controls.base.IFXValidatableControl;
 import edu.wpi.cs3733.d20.teamO.model.database.DatabaseWrapper;
-import edu.wpi.cs3733.d20.teamO.model.datatypes.requests_data.SanitationRequestData;
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
-import java.util.stream.Stream;
 import javafx.fxml.FXML;
 import javafx.scene.control.ToggleGroup;
 import lombok.RequiredArgsConstructor;
-import lombok.val;
 
 @RequiredArgsConstructor(onConstructor_ = {@Inject})
 public class SanitationService extends ViewModelBase {
 
   private final DatabaseWrapper database;
+  //private final Validator validator;
 
   @FXML
   private JFXTextField requesterName;
@@ -29,22 +25,15 @@ public class SanitationService extends ViewModelBase {
   @FXML
   private JFXTextArea additionalNotes;
 
-  /**
-   * @return true if all fields of the view are valid (and can be processed), false otherwise
-   */
-  private boolean allFieldsValid() {
-    return Stream.of(requesterName, floor, location).allMatch(IFXValidatableControl::validate);
-  }
-
   @FXML
   private void submitRequest() {
-    if (allFieldsValid()) {
-      val requestData = new SanitationRequestData(
-          ((JFXRadioButton) levelSelection.getSelectedToggle()).getText(),
-          additionalNotes.getText());
-      // val confirmationCode = database.addServiceRequest()
-      // todo show dialog with confirmation number, or snackbar error
-    }
+//    if (validator.validate(requesterName, floor, location)) {
+//      val requestData = new SanitationRequestData(
+//          ((JFXRadioButton) levelSelection.getSelectedToggle()).getText(),
+//          additionalNotes.getText());
+//      // val confirmationCode = database.addServiceRequest()
+//      // todo show dialog with confirmation number, or snackbar error
+//    }
   }
 
   @FXML
