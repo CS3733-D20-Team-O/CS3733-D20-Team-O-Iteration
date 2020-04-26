43e3506374fedce1f08268bebafd3562a955b166
jacasella <casellaja@gmail.com>
2020-04-26 11:47:03 -0400

    set up tests

--- src/test/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/InternalTransportationTest.java
+++ src/test/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/InternalTransportationTest.java
@@ -1,5 +1,51 @@
 package edu.wpi.cs3733.d20.teamO.view_model.kiosk.service_requests;
 
-public class InternalTransportationTest {
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import edu.wpi.cs3733.d20.teamO.Main;
+import edu.wpi.cs3733.d20.teamO.model.database.DatabaseWrapper;
+import edu.wpi.cs3733.d20.teamO.model.datatypes.Node;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import lombok.val;
+import org.greenrobot.eventbus.EventBus;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+@ExtendWith({MockitoExtension.class, ApplicationExtension.class})
+public class InternalTransportationTest extends FxRobot {
+
+  @Mock
+  EventBus eventBus;
+  @Mock
+  DatabaseWrapper database;
+  @InjectMocks
+  InternalTransportationService ITS;
+
+  @Start
+  public void start(Stage stage) throws IOException {
+    Node n1 = new Node("RHVMNode", 1, 1, 1, "Main", "Test", "TestNode", "testnode");
+
+    val loader = new FXMLLoader();
+    loader.setControllerFactory(o -> ITS);
+    //loader.setResources(bundle);
+    stage.setScene(new Scene(loader.load(Main.class
+        .getResourceAsStream("views/kiosk/service_requests/InternalTransportationService.fxml"))));
+    stage.setAlwaysOnTop(true);
+    stage.show();
+  }
+
+  @Test
+  public void waitToSeeIfItWorks() {
+    sleep(5000);
+    assertTrue(true);
+  }
 }
