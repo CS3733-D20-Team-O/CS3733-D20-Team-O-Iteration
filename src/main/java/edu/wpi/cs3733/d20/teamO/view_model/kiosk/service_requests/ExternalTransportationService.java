71f9cd8b25272ebbb308b8eebde9b793f33789ca
Ryan Stebe <rjstebe1@gmail.com>
2020-04-27 13:03:21 -0400

    Created ExternalTransportationServiceTest and updated other files to match SanitationServiceRequest.

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/ExternalTransportationService.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/ExternalTransportationService.java
@@ -4,11 +4,16 @@ import com.google.inject.Inject;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
-import com.jfoenix.controls.base.IFXValidatableControl;
 import edu.wpi.cs3733.d20.teamO.model.database.DatabaseWrapper;
+import edu.wpi.cs3733.d20.teamO.model.datatypes.Node;
 import edu.wpi.cs3733.d20.teamO.model.datatypes.requests_data.SanitationRequestData;
+import edu.wpi.cs3733.d20.teamO.model.material.Dialog;
+import edu.wpi.cs3733.d20.teamO.model.material.SnackBar;
+import edu.wpi.cs3733.d20.teamO.model.material.Validator;
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
-import java.util.stream.Stream;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
@@ -16,36 +21,73 @@ import lombok.val;
 @RequiredArgsConstructor(onConstructor_ = {@Inject})
 public class ExternalTransportationService extends ViewModelBase {
 
+  //needs: requester name, destination, floor, location, transportation type, additional notes
+
   private final DatabaseWrapper database;
+  private final Validator validator;
+  private final SnackBar snackBar;
+  private final Dialog dialog;
 
   @FXML
   private JFXTextField requesterName, destination;
   @FXML
-  private JFXComboBox<String> floor, location, transportationType;
+  private JFXComboBox<Integer> floors;
+  @FXML
+  private JFXComboBox<String> locations;
+  @FXML
+  private JFXComboBox<String> transportationType;
   @FXML
   private JFXTextArea additionalNotes;
 
-  /**
-   * @return true if all fields of the view are valid (and can be processed), false otherwise
-   */
-  private boolean allFieldsValid() {
-    return Stream.of(requesterName, floor, location, transportationType, destination)
-        .allMatch(IFXValidatableControl::validate);
+  @Override
+  protected void start(URL location, ResourceBundle resources) {
+    // Populate the floors combobox with available nodes
+    database.exportNodes().values().stream()
+        .map(Node::getFloor).distinct().sorted()
+        .forEachOrdered(floors.getItems()::add);
+
+    // Set up the populating of locations on each floor
+    floors.getSelectionModel().selectedItemProperty().addListener((o, oldFloor, newFloor) -> {
+      locations.getItems().clear();
+      database.exportNodes().values().stream()
+          .filter(node -> newFloor.equals(node.getFloor()))
+          .map(Node::getLongName).sorted()
+          .forEachOrdered(locations.getItems()::add);
+      locations.getSelectionModel().select(0);
+    });
+
+    // Preselect the first floor and the first location on that floor
+    if (!floors.getItems().isEmpty()) {
+      floors.getSelectionModel().select(0);
+      locations.getSelectionModel().select(0);
+    }
   }
 
   @FXML
   private void submitRequest() {
-    if (allFieldsValid()) {
+    if (validator.validate(requesterName, floors, locations)) {
       val requestData = new SanitationRequestData(
-          destination.getText(),
+          transportationType.getSelectionModel().getSelectedItem(),
           additionalNotes.getText());
-      // val confirmationCode = database.addServiceRequest()
-      // todo show dialog with confirmation number, or snackbar error
+      val time = LocalDateTime.now().toString(); // todo format this
+      // todo use enum for type of transportation below
+      // todo extract strings
+      val confirmationCode = database.addServiceRequest(
+          time, locations.getSelectionModel().getSelectedItem(),
+          "External Transportation", requesterName.getText(), requestData);
+      if (confirmationCode == null) {
+        snackBar.show("Failed to create the external transportation service request");
+      } else {
+        cancelRequest();
+        dialog.showBasic("External Transportation Request Submitted Successfully",
+            "Your confirmation code is:\n" + confirmationCode, "Close");
+
+      }
     }
   }
 
   @FXML
   private void cancelRequest() {
-    // todo
+    // todo (dialog -> close manual, window, navigator.pop())
   }
 }
