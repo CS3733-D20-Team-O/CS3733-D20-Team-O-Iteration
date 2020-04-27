812e561e34fd05fcb2f1b062a65f1bfcbb98fd16
AzinxtheOnix <mzlai@wpi.edu>
2020-04-27 00:40:06 -0400

    Worked on InfoTech Service and related fxml. Added icons. Made a base test.

---
+++ src/test/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/InfoTechServiceTest.java
@@ -0,0 +1,121 @@
+package edu.wpi.cs3733.d20.teamO.view_model.kiosk.service_requests;
+
+import static org.mockito.Mockito.when;
+import static org.testfx.api.FxAssert.verifyThat;
+
+import edu.wpi.cs3733.d20.teamO.Main;
+import edu.wpi.cs3733.d20.teamO.ResourceBundleMock;
+import edu.wpi.cs3733.d20.teamO.model.database.DatabaseWrapper;
+import edu.wpi.cs3733.d20.teamO.model.datatypes.Node;
+import edu.wpi.cs3733.d20.teamO.model.material.Dialog;
+import edu.wpi.cs3733.d20.teamO.model.material.SnackBar;
+import edu.wpi.cs3733.d20.teamO.model.material.Validator;
+import java.io.IOException;
+import java.util.HashMap;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import lombok.val;
+import org.greenrobot.eventbus.EventBus;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+@ExtendWith({MockitoExtension.class, ApplicationExtension.class})
+public class InfoTechServiceTest extends FxRobot {
+
+  @Mock
+  EventBus eventBus;
+  @Mock
+  DatabaseWrapper database;
+  @Mock
+  Validator validator;
+  @Mock
+  SnackBar snackBar;
+  @Mock
+  Dialog dialog;
+
+  @Spy
+  private final ResourceBundleMock bundle = new ResourceBundleMock();
+
+  @InjectMocks
+  InfoTechService viewModel;
+
+  @Start
+  public void start(Stage stage) throws IOException {
+    bundle.put("Sample", "Sample"); // todo load the necessary strings
+    populateFloorAndLocation();
+    val loader = new FXMLLoader();
+    loader.setControllerFactory(o -> viewModel);
+    loader.setResources(bundle);
+    stage.setScene(new Scene(loader.load(Main.class
+        .getResourceAsStream("views/kiosk/service_requests/SanitationService.fxml"))));
+    stage.setAlwaysOnTop(true);
+    stage.show();
+  }
+
+  private void populateFloorAndLocation() {
+    val map = new HashMap<String, Node>();
+    map.put("a", new Node("a", 0, 0, 1, "", "", "Floor 1", ""));
+    map.put("b", new Node("b", 0, 0, 3, "", "", "Floor 3-1", ""));
+    map.put("c", new Node("c", 0, 0, 3, "", "", "Floor 3-2", ""));
+    map.put("d", new Node("d", 0, 0, 5, "", "", "Floor 5", ""));
+    when(database.exportNodes()).thenReturn(map);
+  }
+
+  @Test
+  public void testFloorLocationPopulated() {
+    // Verify that all floors are populated
+    verifyThat("1", javafx.scene.Node::isVisible);
+    verifyThat("2", n -> !n.isVisible());
+    verifyThat("3", n -> !n.isVisible());
+    verifyThat("4", n -> !n.isVisible());
+    verifyThat("5", n -> !n.isVisible());
+    clickOn("1");
+    verifyThat("1", javafx.scene.Node::isVisible);
+    verifyThat("2", n -> !n.isVisible());
+    verifyThat("3", javafx.scene.Node::isVisible);
+    verifyThat("4", n -> !n.isVisible());
+    verifyThat("5", javafx.scene.Node::isVisible);
+
+    // Now that we know all floors are correct, lets check to see if the locations are present
+    // First floor
+    verifyThat("Floor 1", javafx.scene.Node::isVisible);
+    clickOn("Floor 1");
+    verifyThat("Floor 1", javafx.scene.Node::isVisible);
+    verifyThat("Floor 3-1", n -> !n.isVisible());
+    verifyThat("Floor 3-2", n -> !n.isVisible());
+    verifyThat("Floor 5", n -> !n.isVisible());
+
+    // Third floor
+    clickOn("1");
+    clickOn("3");
+    verifyThat("Floor 3-1", javafx.scene.Node::isVisible);
+    clickOn("Floor 3-1");
+    verifyThat("Floor 1", n -> !n.isVisible());
+    verifyThat("Floor 3-1", javafx.scene.Node::isVisible);
+    verifyThat("Floor 3-2", javafx.scene.Node::isVisible);
+    verifyThat("Floor 5", n -> !n.isVisible());
+
+    // Fifth floor
+    clickOn("3");
+    clickOn("5");
+    verifyThat("Floor 5", javafx.scene.Node::isVisible);
+    clickOn("Floor 5");
+    verifyThat("Floor 1", n -> !n.isVisible());
+    verifyThat("Floor 3-1", n -> !n.isVisible());
+    verifyThat("Floor 3-2", n -> !n.isVisible());
+    verifyThat("Floor 5", javafx.scene.Node::isVisible);
+  }
+
+  @Test
+  public void testSubmitITService() {
+
+  }
+}
