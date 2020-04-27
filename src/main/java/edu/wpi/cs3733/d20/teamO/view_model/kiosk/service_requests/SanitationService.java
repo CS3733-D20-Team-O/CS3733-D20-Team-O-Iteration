7f7847f84d3cf8cf6a8ae8a5cc0003ea9037fa19
GregoryConrad <gregorysconrad@gmail.com>
2020-04-27 18:19:34 -0400

    Added service request base and SanitationService essentially done

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/SanitationService.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/SanitationService.java
@@ -6,12 +6,10 @@ import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733.d20.teamO.model.database.DatabaseWrapper;
-import edu.wpi.cs3733.d20.teamO.model.datatypes.Node;
 import edu.wpi.cs3733.d20.teamO.model.datatypes.requests_data.SanitationRequestData;
 import edu.wpi.cs3733.d20.teamO.model.material.Dialog;
 import edu.wpi.cs3733.d20.teamO.model.material.SnackBar;
 import edu.wpi.cs3733.d20.teamO.model.material.Validator;
-import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.ResourceBundle;
@@ -21,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.val;
 
 @RequiredArgsConstructor(onConstructor_ = {@Inject})
-public class SanitationService extends ViewModelBase {
+public class SanitationService extends ServiceRequestBase {
 
   private final DatabaseWrapper database;
   private final Validator validator;
@@ -41,19 +39,7 @@ public class SanitationService extends ViewModelBase {
 
   @Override
   protected void start(URL location, ResourceBundle resources) {
-    // Populate the floors combobox with available nodes
-    database.exportNodes().values().stream()
-        .map(Node::getFloor).distinct().sorted()
-        .forEachOrdered(floors.getItems()::add);
-
-    // Set up the populating of locations on each floor
-    floors.getSelectionModel().selectedItemProperty().addListener((o, oldFloor, newFloor) -> {
-      locations.getItems().clear();
-      database.exportNodes().values().stream()
-          .filter(node -> newFloor.equals(node.getFloor()))
-          .map(Node::getLongName).sorted()
-          .forEachOrdered(locations.getItems()::add);
-    });
+    setLocations(floors, locations);
   }
 
   @FXML
@@ -71,16 +57,10 @@ public class SanitationService extends ViewModelBase {
       if (confirmationCode == null) {
         snackBar.show("Failed to create the sanitation service request");
       } else {
-        closeRequest();
+        close();
         dialog.showBasic("Sanitation Request Submitted Successfully",
             "Your confirmation code is:\n" + confirmationCode, "Close");
-
       }
     }
-  }
-
-  @FXML
-  private void closeRequest() {
-    // todo (dialog -> close manual, window, navigator.pop())
   }
 }
