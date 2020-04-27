5a2757e2b5ae7edd78d211ada5d819e1169b52bd
Victoria Grasso <vygrasso@wpi.edu>
2020-04-26 23:24:04 -0400

    Fixes #174

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/InterpreterService.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/InterpreterService.java
@@ -5,22 +5,95 @@ import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733.d20.teamO.model.database.DatabaseWrapper;
+import edu.wpi.cs3733.d20.teamO.model.datatypes.Node;
+import edu.wpi.cs3733.d20.teamO.model.datatypes.requests_data.InterpreterData;
+import edu.wpi.cs3733.d20.teamO.model.material.Dialog;
+import edu.wpi.cs3733.d20.teamO.model.material.SnackBar;
+import edu.wpi.cs3733.d20.teamO.model.material.Validator;
 import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.ToggleGroup;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 
 @RequiredArgsConstructor(onConstructor_ = {@Inject})
 public class InterpreterService extends ViewModelBase {
 
   private final DatabaseWrapper database;
+  private final Validator validator;
+  private final SnackBar snackBar;
+  private final Dialog dialog;
 
   @FXML
   private JFXTextField requesterName;
   @FXML
-  private JFXComboBox<String> floor, location;
+  private JFXComboBox<Integer> floors;
   @FXML
-  private ToggleGroup levelSelection;
+  private JFXComboBox<String> locations;
+  @FXML
+  private JFXComboBox<String> language;
+  @FXML
+  private JFXComboBox<String> gender;
   @FXML
   private JFXTextArea additionalNotes;
+
+  @Override
+  protected void start(URL location, ResourceBundle resources) {
+    // Populate the floors combobox with available nodes
+    database.exportNodes().values().stream()
+        .map(Node::getFloor).distinct().sorted()
+        .forEachOrdered(floors.getItems()::add);
+
+    // Set up the populating of locations on each floor
+    floors.getSelectionModel().selectedItemProperty().addListener((o, oldFloor, newFloor) ->
+        database.exportNodes().values().stream()
+            .filter(node -> newFloor.equals(node.getFloor()))
+            .map(Node::getLongName).sorted()
+            .forEachOrdered(locations.getItems()::add));
+
+    language.getItems().add("Spanish");
+    language.getItems().add("French");
+    language.getItems().add("Chinese");
+    language.getItems().add("Japanese");
+    language.getItems().add("Russian");
+
+    gender.getItems().add("Male");
+    gender.getItems().add("Female");
+    gender.getItems().add("No Preference");
+
+    // Preselect the first floor and the first location on that floor
+    if (!floors.getItems().isEmpty()) {
+      floors.getSelectionModel().select(0);
+      locations.getSelectionModel().select(0);
+    }
+  }
+
+  @FXML
+  private void submitRequest() {
+    if (validator.validate(requesterName, floors, locations, language, gender)) {
+      val requestData = new InterpreterData(language.getValue(), gender.getValue(),
+          additionalNotes.getText());
+      val time = LocalDateTime.now().toString(); // todo format this
+      // todo use enum for sanitation string below
+      // todo extract strings
+      val confirmationCode = database.addServiceRequest(
+          time, locations.getSelectionModel().getSelectedItem(),
+          "Sanitation", requesterName.getText(), requestData);
+      if (confirmationCode == null) {
+        snackBar.show("Failed to create the sanitation service request");
+      } else {
+        closeRequest();
+        dialog.showBasic("Sanitation Request Submitted Successfully",
+            "Your confirmation code is:\n" + confirmationCode, "Close");
+
+      }
+    }
+  }
+
+  @FXML
+  private void closeRequest() {
+    // todo (dialog -> close manual, window, navigator.pop())
+  }
 }
