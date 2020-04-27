e169992b61581c2b2a6ee8de08a0a4b6ca264189
GregoryConrad <gregorysconrad@gmail.com>
2020-04-26 20:33:38 -0400

    Added template to the sanitation test

--- src/test/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/SanitationServiceTest.java
+++ src/test/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/SanitationServiceTest.java
@@ -1,5 +1,62 @@
 package edu.wpi.cs3733.d20.teamO.view_model.kiosk.service_requests;
 
-public class SanitationServiceTest {
+import edu.wpi.cs3733.d20.teamO.Main;
+import edu.wpi.cs3733.d20.teamO.ResourceBundleMock;
+import edu.wpi.cs3733.d20.teamO.model.database.DatabaseWrapper;
+import edu.wpi.cs3733.d20.teamO.model.material.Dialog;
+import edu.wpi.cs3733.d20.teamO.model.material.SnackBar;
+import edu.wpi.cs3733.d20.teamO.model.material.Validator;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import lombok.val;
+import org.greenrobot.eventbus.EventBus;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
 
+@ExtendWith({MockitoExtension.class, ApplicationExtension.class})
+public class SanitationServiceTest extends FxRobot {
+
+  @Mock
+  EventBus eventBus;
+  @Mock
+  DatabaseWrapper database;
+  @Mock
+  Validator validator;
+  @Mock
+  SnackBar snackBar;
+  @Mock
+  Dialog dialog;
+
+  @Spy
+  private final ResourceBundleMock bundle = new ResourceBundleMock();
+
+  @InjectMocks
+  SanitationService viewModel;
+
+  @Start
+  public void start(Stage stage) throws IOException {
+    // Add the necessary strings to the bundle
+    bundle.put("Sample", "Sample");
+
+    // Load the actual test environment
+    val loader = new FXMLLoader();
+    loader.setControllerFactory(o -> viewModel);
+    loader.setResources(bundle);
+    stage.setScene(new Scene(loader.load(Main.class
+        .getResourceAsStream("views/kiosk/service_requests/SanitationService.fxml"))));
+    stage.setAlwaysOnTop(true);
+    stage.show();
+  }
+
+  // todo check for floor & location populated
+  // todo check submit
+  // todo check close
 }
