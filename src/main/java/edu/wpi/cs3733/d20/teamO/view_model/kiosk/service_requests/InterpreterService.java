ea35ee062c5bf7dacaa65d72d6bd66028eeba061
Victoria Grasso <vygrasso@wpi.edu>
2020-04-26 22:35:27 -0400

    Fixes #174

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/InterpreterService.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/InterpreterService.java
@@ -1,5 +1,26 @@
 package edu.wpi.cs3733.d20.teamO.view_model.kiosk.service_requests;
 
-public class InterpreterService {
+import com.google.inject.Inject;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import edu.wpi.cs3733.d20.teamO.model.database.DatabaseWrapper;
+import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor(onConstructor_ = {@Inject})
+public class InterpreterService extends ViewModelBase {
+
+  private final DatabaseWrapper database;
+
+  @FXML
+  private JFXTextField requesterName;
+  @FXML
+  private JFXComboBox<String> floor, location;
+  @FXML
+  private ToggleGroup levelSelection;
+  @FXML
+  private JFXTextArea additionalNotes;
 }
