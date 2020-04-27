c6f8ed531b21b86adab968245f41e6f53b49cefa
GregoryConrad <gregorysconrad@gmail.com>
2020-04-26 23:55:46 -0400

    Fixes tests and bugs

--- src/test/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/SanitationServiceTest.java
+++ src/test/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/SanitationServiceTest.java
@@ -2,6 +2,7 @@ package edu.wpi.cs3733.d20.teamO.view_model.kiosk.service_requests;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -77,68 +78,61 @@ public class SanitationServiceTest extends FxRobot {
   @Test
   public void testFloorLocationPopulated() {
     // Verify that all floors are populated
-    verifyThat("1", javafx.scene.Node::isVisible);
-    verifyThat("2", n -> !n.isVisible());
-    verifyThat("3", n -> !n.isVisible());
-    verifyThat("4", n -> !n.isVisible());
-    verifyThat("5", n -> !n.isVisible());
     clickOn("1");
     verifyThat("1", javafx.scene.Node::isVisible);
-    verifyThat("2", n -> !n.isVisible());
     verifyThat("3", javafx.scene.Node::isVisible);
-    verifyThat("4", n -> !n.isVisible());
     verifyThat("5", javafx.scene.Node::isVisible);
 
     // Now that we know all floors are correct, lets check to see if the locations are present
     // First floor
-    verifyThat("Floor 1", javafx.scene.Node::isVisible);
     clickOn("Floor 1");
     verifyThat("Floor 1", javafx.scene.Node::isVisible);
-    verifyThat("Floor 3-1", n -> !n.isVisible());
-    verifyThat("Floor 3-2", n -> !n.isVisible());
-    verifyThat("Floor 5", n -> !n.isVisible());
 
     // Third floor
     clickOn("1");
     clickOn("3");
-    verifyThat("Floor 3-1", javafx.scene.Node::isVisible);
     clickOn("Floor 3-1");
-    verifyThat("Floor 1", n -> !n.isVisible());
     verifyThat("Floor 3-1", javafx.scene.Node::isVisible);
     verifyThat("Floor 3-2", javafx.scene.Node::isVisible);
-    verifyThat("Floor 5", n -> !n.isVisible());
 
     // Fifth floor
     clickOn("3");
     clickOn("5");
     verifyThat("Floor 5", javafx.scene.Node::isVisible);
-    clickOn("Floor 5");
-    verifyThat("Floor 1", n -> !n.isVisible());
-    verifyThat("Floor 3-1", n -> !n.isVisible());
-    verifyThat("Floor 3-2", n -> !n.isVisible());
-    verifyThat("Floor 5", javafx.scene.Node::isVisible);
   }
 
   @Test
   public void testSubmit() {
-    when(validator.validate(any())).thenReturn(false).thenReturn(true);
-    when(database.addServiceRequest(any(), any(), any(), any(), any())).thenReturn("ABCDEFGH");
+    when(validator.validate(any())).thenReturn(false).thenReturn(true).thenReturn(true);
+    when(database.addServiceRequest(any(), any(), any(), any(), any()))
+        .thenReturn(null).thenReturn("ABCDEFGH");
+
     // Test when there are fields not filled out
     clickOn("Submit");
     verify(validator, times(1)).validate(any());
     verify(database, times(0)).addServiceRequest(any(), any(), any(), any(), any());
-    verify(snackBar, times(1)).show(anyString());
+    verify(snackBar, times(0)).show(anyString());
     verify(dialog, times(0)).showBasic(any(), any(), any());
-    // Test when there are fields filled out
+
+    // Test when there are fields filled out (but adding fails)
     clickOn("Your Name");
     write("John Smith");
     clickOn("Submit");
     verify(validator, times(2)).validate(any());
     verify(database, times(1)).addServiceRequest(anyString(),
-        "Floor 1", "Sanitation", "John Smith",
-        new SanitationRequestData("Dry Spill", ""));
+        eq("Floor 1"), eq("Sanitation"), eq("John Smith"),
+        eq(new SanitationRequestData("Dry Spill", "")));
+    verify(snackBar, times(1)).show(anyString());
+    verify(dialog, times(0)).showBasic(any(), any(), any());
+
+    // Test when there are fields filled out (and adding succeeds)
+    clickOn("Submit");
+    verify(validator, times(3)).validate(any());
+    verify(database, times(2)).addServiceRequest(anyString(),
+        eq("Floor 1"), eq("Sanitation"), eq("John Smith"),
+        eq(new SanitationRequestData("Dry Spill", "")));
     verify(snackBar, times(1)).show(anyString());
     verify(dialog, times(1))
-        .showBasic(any(), "Your confirmation code is:\nABCDEFGH", any());
+        .showBasic(anyString(), eq("Your confirmation code is:\nABCDEFGH"), anyString());
   }
 }
