47476bf4581287c19916fe09f2234587e3233515
Collin Broderick <sedrahedron@gmail.com>
2020-04-28 13:39:04 -0400

    More functionality updates

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/NodeMapView.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/NodeMapView.java
@@ -360,7 +360,7 @@ public class NodeMapView extends ViewModelBase {
   public void setFloor(int floor) {
     if (floor >= minFloor && floor <= maxFloor) {
       this.floor = floor;
-      backgroundImage.setImage(new Image("floorNumber/" + floor + ".png"));
+      backgroundImage.setImage(new Image("floors/" + floor + ".png"));
     }
     draw();
   }
