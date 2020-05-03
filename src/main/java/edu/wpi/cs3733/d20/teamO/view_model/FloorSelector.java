1dc08e86e4cd4c57a79da36c172aaa7fe9e67a90
Collin Broderick <sedrahedron@gmail.com>
2020-05-02 22:55:43 -0400

    Revert "Finished functionality"
    This reverts commit c2f21ff6

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/FloorSelector.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/FloorSelector.java
@@ -10,50 +10,58 @@ public class FloorSelector extends ViewModelBase {
   private NodeMapView nodeMapViewController;
   @FXML
   private JFXButton floor1Btn, floor2Btn, floor3Btn, floor4Btn, floor5Btn;
-  private JFXButton[] buttons = {floor1Btn, floor2Btn, floor3Btn, floor4Btn, floor5Btn};
 
   /**
    * Sets the nodeMapViewController
    *
-   * @param controller The controller
+   * @param nodeMapViewController The controller
    */
-  public void setNodeMapViewController(NodeMapView controller) {
-    nodeMapViewController = controller;
-  }
-
-  /**
-   * Sets the floor of the application
-   *
-   * @param floor The floor to set
-   */
-  private void setFloor(int floor) {
-    nodeMapViewController.setFloor(floor);
-    for (int i = 0; i < buttons.length; i++) {
-      if (i + 1 == floor) {
-        buttons[i].setDisable(true);
-      } else {
-        buttons[i].setDisable(false);
-      }
-    }
+  public void setNodeMapViewController(NodeMapView nodeMapViewController) {
+    this.nodeMapViewController = nodeMapViewController;
   }
 
   public void floor1Select(ActionEvent event) {
-    setFloor(1);
+    nodeMapViewController.setFloor(1);
+    floor1Btn.setDisable(true);
+    floor2Btn.setDisable(false);
+    floor3Btn.setDisable(false);
+    floor4Btn.setDisable(false);
+    floor5Btn.setDisable(false);
   }
 
   public void floor2Select(ActionEvent event) {
-    setFloor(2);
+    nodeMapViewController.setFloor(2);
+    floor1Btn.setDisable(false);
+    floor2Btn.setDisable(true);
+    floor3Btn.setDisable(false);
+    floor4Btn.setDisable(false);
+    floor5Btn.setDisable(false);
   }
 
   public void floor3Select(ActionEvent event) {
-    setFloor(3);
+    nodeMapViewController.setFloor(3);
+    floor1Btn.setDisable(false);
+    floor2Btn.setDisable(false);
+    floor3Btn.setDisable(true);
+    floor4Btn.setDisable(false);
+    floor5Btn.setDisable(false);
   }
 
   public void floor4Select(ActionEvent event) {
-    setFloor(4);
+    nodeMapViewController.setFloor(4);
+    floor1Btn.setDisable(false);
+    floor2Btn.setDisable(false);
+    floor3Btn.setDisable(false);
+    floor4Btn.setDisable(true);
+    floor5Btn.setDisable(false);
   }
 
   public void floor5Select(ActionEvent event) {
-    setFloor(5);
+    nodeMapViewController.setFloor(5);
+    floor1Btn.setDisable(false);
+    floor2Btn.setDisable(false);
+    floor3Btn.setDisable(false);
+    floor4Btn.setDisable(false);
+    floor5Btn.setDisable(true);
   }
 }
