30f34569dde7a9b894daabc9e62229de7c430b95
Collin Broderick <sedrahedron@gmail.com>
2020-05-04 15:30:50 -0400

    Worked on ViewModel

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/ZoomBar.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/ZoomBar.java
@@ -1,5 +1,16 @@
 package edu.wpi.cs3733.d20.teamO.view_model;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class ZoomBar extends ViewModelBase {
 
+  public void zoomOut(ActionEvent event) {
+  }
+
+  public void setZoom(MouseEvent mouseEvent) {
+  }
+
+  public void zoomIn(ActionEvent event) {
+  }
 }
