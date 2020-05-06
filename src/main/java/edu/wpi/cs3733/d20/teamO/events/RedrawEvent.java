7c5aee0b5e24c49e5a0ca1a3d38c202601788956
jacasella <casellaja@gmail.com>
2020-05-05 23:59:36 -0400

    Made RedrawEvent implement Event

--- src/main/java/edu/wpi/cs3733/d20/teamO/events/RedrawEvent.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/events/RedrawEvent.java
@@ -3,6 +3,6 @@ package edu.wpi.cs3733.d20.teamO.events;
 /**
  * Event called when the floor/building is switched and the map needs to be redrawn
  */
-public class RedrawEvent {
+public class RedrawEvent implements Event {
 
 }
