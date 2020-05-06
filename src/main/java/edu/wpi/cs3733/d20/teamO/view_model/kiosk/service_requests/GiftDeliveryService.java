e29dea100b0d5f5044de31775cda76bfad060ca5
Victoria Grasso <vygrasso@wpi.edu>
2020-05-05 22:46:15 -0400

    Gift fixes to node selector

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/GiftDeliveryService.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/service_requests/GiftDeliveryService.java
@@ -10,10 +10,10 @@ import edu.wpi.cs3733.d20.teamO.model.datatypes.requests_data.GiftDeliveryReques
 import edu.wpi.cs3733.d20.teamO.model.material.Dialog;
 import edu.wpi.cs3733.d20.teamO.model.material.SnackBar;
 import edu.wpi.cs3733.d20.teamO.model.material.Validator;
+import edu.wpi.cs3733.d20.teamO.model.material.node_selector.NodeSelector;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 import javafx.fxml.FXML;
@@ -37,9 +37,11 @@ public class GiftDeliveryService extends ServiceRequestBase {
   private JFXTextField toField, fromField, ccNumberField,
       ccSecurityField, ccFirstNameField, ccLastNameField, emailAddressField;
   @FXML
-  private JFXComboBox<String> inRoomComboBox, onFloorComboBox, ccTypeComboBox,
+  private JFXComboBox<String> ccTypeComboBox,
       ccMonthComboBox, ccYearComboBox,
       giftComboBox;
+  @FXML
+  private NodeSelector nodeSelector;
 
   @FXML
   private Label totalLabel;
@@ -49,7 +51,8 @@ public class GiftDeliveryService extends ServiceRequestBase {
 
   @Override
   protected void start(URL location, ResourceBundle resources) {
-    inRoomComboBox.setDisable(true);
+    //inRoomComboBox.setDisable(true);
+    nodeSelector.setNodes(database.exportNodes().values());
     addComboBoxOptions(); //adds Floors, Rooms
   }
 
@@ -65,22 +68,22 @@ public class GiftDeliveryService extends ServiceRequestBase {
   }
 
   private void addComboBoxOptions() {
-    onFloorComboBox.getItems().addAll("1", "2", "3", "4", "5");
+    //onFloorComboBox.getItems().addAll("1", "2", "3", "4", "5");
 
-    //sets rooms
-    for (Map.Entry<String, Node> node : database.exportNodes().entrySet()) {
-      val roomToAdd = node.getValue().getLongName();
-      val roomNode = node.getValue();
-      //ignores non-valid rooms
-      if (!listOfRooms.contains(roomToAdd) &&
-          !(node.getValue().getNodeType().equals("STAI")) &&
-          !(node.getValue().getNodeType().equals("ELEV")) &&
-          !(node.getValue().getNodeType().equals("REST")) &&
-          !(node.getValue().getNodeType().equals("HALL"))) {
-        listOfRooms.add(roomNode);
-        inRoomComboBox.getItems().add(roomToAdd);
-      }
-    }
+//    //sets rooms
+//    for (Map.Entry<String, Node> node : database.exportNodes().entrySet()) {
+//      val roomToAdd = node.getValue().getLongName();
+//      val roomNode = node.getValue();
+//      //ignores non-valid rooms
+//      if (!listOfRooms.contains(roomToAdd) &&
+//          !(node.getValue().getNodeType().equals("STAI")) &&
+//          !(node.getValue().getNodeType().equals("ELEV")) &&
+//          !(node.getValue().getNodeType().equals("REST")) &&
+//          !(node.getValue().getNodeType().equals("HALL"))) {
+//        listOfRooms.add(roomNode);
+//        inRoomComboBox.getItems().add(roomToAdd);
+//      }
+//    }
 
     //sets CC YYYY
     val curYear = Calendar.getInstance().get(Calendar.YEAR);
@@ -94,26 +97,25 @@ public class GiftDeliveryService extends ServiceRequestBase {
   /**
    * Only shows available rooms when a floor is selected from the Floor combo box
    */
-  @FXML
-  private void setRoomsByFloors() {
-    val currentFloor = onFloorComboBox.getSelectionModel().getSelectedItem();
-    inRoomComboBox.getItems().clear();
-    for (val node : listOfRooms) {
-      if ((node.getFloor()).equals(currentFloor)) {
-        inRoomComboBox.getItems().add(node.getLongName());
-      }
-    }
-    inRoomComboBox.setDisable(false);
-    inRoomComboBox.getSelectionModel().selectFirst();
-    if (inRoomComboBox.getSelectionModel().isEmpty()) {
-      inRoomComboBox.setDisable(true);
-    }
-  }
-
+//  @FXML
+//  private void setRoomsByFloors() {
+//    val currentFloor = onFloorComboBox.getSelectionModel().getSelectedItem();
+//    inRoomComboBox.getItems().clear();
+//    for (val node : listOfRooms) {
+//      if ((node.getFloor()).equals(currentFloor)) {
+//        inRoomComboBox.getItems().add(node.getLongName());
+//      }
+//    }
+//    inRoomComboBox.setDisable(false);
+//    inRoomComboBox.getSelectionModel().selectFirst();
+//    if (inRoomComboBox.getSelectionModel().isEmpty()) {
+//      inRoomComboBox.setDisable(true);
+//    }
+//  }
   @FXML
   private void onSubmitPress() {
     if (!validator.validate(giftComboBox, toField, fromField,
-        onFloorComboBox, inRoomComboBox, timePicker,
+        nodeSelector, timePicker,
         ccFirstNameField, ccLastNameField,
         ccTypeComboBox, ccMonthComboBox, ccYearComboBox)) {
       dialog.showBasic("Missing Information",
@@ -149,7 +151,7 @@ public class GiftDeliveryService extends ServiceRequestBase {
 
     val confirmationCode = database.addServiceRequest(
         timePicker.getValue().toString(),
-        inRoomComboBox.getSelectionModel().getSelectedItem(),
+        nodeSelector.getSelectedNode().getLongName(),
         "Gift",
         fromField.getText(),
         requestedData);
