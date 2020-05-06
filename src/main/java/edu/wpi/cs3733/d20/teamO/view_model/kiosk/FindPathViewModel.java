8d11d2ebea09219ff2651d133919b55b7734e629
AzinxtheOnix <mzlai@wpi.edu>
2020-05-06 13:17:06 -0400

    Fixed a null pointer with the color picker

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/FindPathViewModel.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/FindPathViewModel.java
@@ -65,7 +65,7 @@ public class FindPathViewModel extends ViewModelBase {
   private Node beginning, finish, defaultStart, defaultStop;
   private final Dialog dialog;
   private final SnackBar snackBar;
-  private Color color;
+  private Color color = Color.web("fd8842");
 
   private final SelectedPathFinder pathFinder;
 
