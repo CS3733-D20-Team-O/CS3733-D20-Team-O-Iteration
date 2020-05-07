071b77bcd23e2affdd0e5912bce7a1699cc1e0c5
GregoryConrad <gregorysconrad@gmail.com>
2020-05-07 13:53:55 -0400

    Added basic (subject to change) interface for street view

---
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/StreetViewViewModel.java
@@ -0,0 +1,24 @@
+package edu.wpi.cs3733.d20.teamO.view_model;
+
+import javafx.fxml.FXML;
+import javafx.scene.web.WebView;
+
+public class StreetViewViewModel extends ViewModelBase {
+
+  @FXML
+  private WebView webView;
+
+  /**
+   * Switches the directions to go to faulkner from main campus
+   */
+  public void goToFaulkner() {
+    // todo
+  }
+
+  /**
+   * Switches the directions to go to main campus from faulkner
+   */
+  public void goToMainCampus() {
+    // todo
+  }
+}
