a1ab226d1cece1b39fe6413d2fa7b201bfe69224
Benny Klaiman <bnklaiman@wpi.edu>
2020-05-08 17:41:25 -0400

    undid the last commit, this was a mistake

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/AboutViewModel.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/kiosk/AboutViewModel.java
@@ -1,18 +1,7 @@
 package edu.wpi.cs3733.d20.teamO.view_model.kiosk;
 
 import edu.wpi.cs3733.d20.teamO.model.material.Dialog.DialogViewModel;
-import java.io.IOException;
-import javafx.fxml.FXML;
 
 public class AboutViewModel extends DialogViewModel {
-
-  @FXML
-  private void openCredits() {
-    try {
-      dialog.showFullscreenFXML("views/kiosk/SoftwareUsed.fxml");
-    } catch (IOException e) {
-      log.error("Could not load the software used page", e);
-    }
-  }
 
 }
