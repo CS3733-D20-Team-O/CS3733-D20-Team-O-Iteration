ba7c703a59a63c29a754d7d3c6679d19c68e762b
Mark Hogan <mhogan2@wpi.edu>
2020-05-08 23:11:34 -0400

    Clock changes

--- src/main/java/edu/wpi/cs3733/d20/teamO/Clock.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/Clock.java
@@ -2,6 +2,7 @@ package edu.wpi.cs3733.d20.teamO;
 
 
 import java.time.LocalTime;
+import java.util.ResourceBundle;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -14,7 +15,8 @@ public class Clock {
   @FXML
   private Label time;
 
-  public void start() {
+  @Override
+  protected void start(URL url, ResourceBundle resources) {
     Timeline clock = new Timeline(
         new KeyFrame(Duration.ZERO, e -> {
           LocalTime currentTime = LocalTime.now();
