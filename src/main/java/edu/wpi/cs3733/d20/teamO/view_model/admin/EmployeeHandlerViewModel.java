ccbabb062b1d17d493f6f4d1fdbbab916c68dd85
Victoria Grasso <vygrasso@wpi.edu>
2020-05-09 22:28:55 -0400

    Fixes #295

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/EmployeeHandlerViewModel.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/admin/EmployeeHandlerViewModel.java
@@ -1,5 +1,34 @@
 package edu.wpi.cs3733.d20.teamO.view_model.admin;
 
-public class EmployeeHandlerViewModel {
+
+import com.google.inject.Inject;
+import com.jfoenix.controls.JFXButton;
+import edu.wpi.cs3733.d20.teamO.model.database.DatabaseWrapper;
+import edu.wpi.cs3733.d20.teamO.model.datatypes.Employee;
+import edu.wpi.cs3733.d20.teamO.model.datatypes.LoginDetails;
+import edu.wpi.cs3733.d20.teamO.model.material.SnackBar;
+import edu.wpi.cs3733.d20.teamO.view_model.ViewModelBase;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(onConstructor_ = {@Inject})
+public class EmployeeHandlerViewModel extends ViewModelBase {
+
+  private final DatabaseWrapper database;
+  private final LoginDetails loginDetails;
+  private final SnackBar snackBar;
+
+  @FXML
+  private TableView<Employee> employeeTable;
+  @FXML
+  private JFXButton addEmp, deleteEmp, updateEmp;
+
+  @Override
+  protected void start(URL location, ResourceBundle resources) {
+    employeeTable.getColumns().remove(3); //4th col
+  }
 
 }
