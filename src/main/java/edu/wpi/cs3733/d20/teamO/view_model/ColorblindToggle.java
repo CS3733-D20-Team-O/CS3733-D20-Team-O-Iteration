a1164ec74191502b3ced7cbeaf7c0881f8488f11
Collin Broderick <sedrahedron@gmail.com>
2020-05-11 21:22:02 -0400

    Programmed ViewModel to throw CSSSwitchEvent

--- src/main/java/edu/wpi/cs3733/d20/teamO/view_model/ColorblindToggle.java
+++ src/main/java/edu/wpi/cs3733/d20/teamO/view_model/ColorblindToggle.java
@@ -1,16 +1,37 @@
 package edu.wpi.cs3733.d20.teamO.view_model;
 
 import com.jfoenix.controls.JFXComboBox;
+import edu.wpi.cs3733.d20.teamO.events.CSSSwitchEvent;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
-public class ColorblindToggle {
+public class ColorblindToggle extends ViewModelBase {
 
   private static final String normalVisionCSSPath = "";
+  private static final String deuteranopiaCSSPath = "";
+  private static final String protanopiaCSSPath = "";
+  private static final String tritanopiaCSSPath = "";
   @FXML
-  private JFXComboBox<String> colorPicker;
+  private JFXComboBox<Label> colorPicker;
 
   public void selectColor(ActionEvent event) {
-
+    String selection = colorPicker.getSelectionModel().getSelectedItem().getText();
+    String path = "";
+    switch (selection) {
+      case "Normal Vision":
+        path = normalVisionCSSPath;
+        break;
+      case "Deuteranopia":
+        path = deuteranopiaCSSPath;
+        break;
+      case "Protanopia":
+        path = protanopiaCSSPath;
+        break;
+      case "Tritanopia":
+        path = tritanopiaCSSPath;
+        break;
+    }
+    dispatch(new CSSSwitchEvent(path));
   }
 }
